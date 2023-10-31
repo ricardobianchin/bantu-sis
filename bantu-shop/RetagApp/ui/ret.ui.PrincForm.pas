184cf794be0d89a63caa75c3b4803519e9a761b9
Ricardo Bianchin <ricardobianchin@gmail.com>
2023-10-31 15:53:09 -0300

    br-teste princform

--- bantu-shop/RetagApp/ui/ret.ui.PrincForm.pas
+++ bantu-shop/RetagApp/ui/ret.ui.PrincForm.pas
@@ -4,11 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Btu.UI.BasForm.Princ_u, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Btu.UI.BasForm.Princ_u, Vcl.ExtCtrls,
+  Vcl.StdCtrls;
 
 type
   TRetagPrincForm = class(TPrincBasForm)
     CloseTimer: TTimer;
+    Label1: TLabel;
     procedure ShowTimerBasTimer(Sender: TObject);
     procedure CloseTimerTimer(Sender: TObject);
   private
