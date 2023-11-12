feb734e19240bdc53700761f1a1c31c8f18265c2
Ricardo Bianchin <ricardobianchin@gmail.com>
2023-11-12 14:53:20 -0300

    ft-adm-inicial cria bpls, funcionando ok

---
+++ App/Testes/App.Teste.UI.IO.Outputs.pas
@@ -0,0 +1,16 @@
+unit App.Teste.UI.IO.Outputs;
+
+interface
+
+procedure Teste;
+
+implementation
+
+uses Sis.Teste.UI.IO.Outputs;
+
+procedure Teste;
+begin
+  Sis.Teste.UI.IO.Outputs.SimunlaLabelSetCaption('Teste');
+end;
+
+end.
---
+++ AppShop/TesteTmp_u.pas
@@ -0,0 +1,16 @@
+unit TesteTmp_u;
+
+interface
+
+procedure Teste;
+
+implementation
+
+uses App.Teste.UI.IO.Outputs;
+
+procedure Teste;
+begin
+  App.Teste.UI.IO.Outputs.Teste;
+end;
+
+end.
