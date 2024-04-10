0dcf7a7f4dbe0e8f474a19e2d804b23bc925cd14
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-04-10 16:11:16 -0300

    ft-pag hashitem format source

--- Sis/Lists/Sis.Lists.HashItem.pas
+++ Sis/Lists/Sis.Lists.HashItem.pas
@@ -5,13 +5,13 @@ interface
 uses Sis.Lists.IdItem, Vcl.Controls;
 
 type
-  IHashItem=interface(IIdItem)
+  IHashItem = interface(IIdItem)
     ['{6F65B9D7-4ED6-4D94-B886-CBBE7C6B12EB}']
     procedure Zerar;
 
-    function GetDescr:string;
-    procedure SetDescr(const Value:string);
-    property Descr:string read GetDescr write SetDescr;
+    function GetDescr: string;
+    procedure SetDescr(const Value: string);
+    property Descr: string read GetDescr write SetDescr;
 
     function GetAsStringCSV: string;
     property AsStringCSV: string read GetAsStringCSV;
