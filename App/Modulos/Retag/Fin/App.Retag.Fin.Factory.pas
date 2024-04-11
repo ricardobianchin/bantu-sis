eff2bbe909f3e487fbfe2a255fde4b313209da66
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-04-10 22:14:10 -0300

    ft-pag dbi cast

--- App/Modulos/Retag/Fin/App.Retag.Fin.Factory.pas
+++ App/Modulos/Retag/Fin/App.Retag.Fin.Factory.pas
@@ -2,17 +2,29 @@ unit App.Retag.Fin.Factory;
 
 interface
 
-uses App.FIn.PagFormaTipo;
+uses App.FIn.PagFormaTipo, App.Ent.Ed, App.Ent.DBI;
 
 function PagFormaTipoCreate: IPagFormaTipo;
+function EntEdCastToFormaTipoEnt(pEntEd: IEntEd): IPagFormaTipo;
+function EntDBICastToFormaTipoDBI(pEntDBI: IEntDBI): IEntDBI;
 
 implementation
 
-uses App.FIn.PagFormaTipo_u;
+uses App.FIn.PagFormaTipo_u, App.Retag.Fin.PagForma.DBI_u;
 
 function PagFormaTipoCreate: IPagFormaTipo;
 begin
   Result := TPagFormaTipo.Create();
+end;
+
+function EntEdCastToFormaTipoEnt(pEntEd: IEntEd): IPagFormaTipo;
+begin
+  Result := TPagFormaTipo(pEntEd);
+end;
+
+function EntDBICastToFormaTipoDBI(pEntDBI: IEntDBI): IEntDBI;
+begin
+  Result := TPagFormaDBI(pEntDBI);
 end;
 
 end.
