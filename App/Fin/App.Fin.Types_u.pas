92d639247d6e0fc753870871990d2398c0c9aefc
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-04-13 22:18:07 -0300

    ft-pag consts PAG_FORMA_TIPO...

--- App/Fin/App.Fin.Types_u.pas
+++ App/Fin/App.Fin.Types_u.pas
@@ -2,6 +2,11 @@ unit App.Fin.Types_u;
 
 interface
 
+const
+  PAG_FORMA_TIPO_DINHEIRO = 33;
+  PAG_FORMA_TIPO_DEBITO = 34;
+  PAG_FORMA_TIPO_CREDITO = 35;
+  PAG_FORMA_TIPO_TRANSFERENCIA = 36;
 //type
 
 
