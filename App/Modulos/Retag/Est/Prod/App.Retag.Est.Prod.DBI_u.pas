527cb720f9f928348bc3fc3bad9027279ce2aea9
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-04-14 10:12:11 -0300

    ft-pag-ent-insert TProdDBI ent sobe, cria inserir

--- App/Modulos/Retag/Est/Prod/App.Retag.Est.Prod.DBI_u.pas
+++ App/Modulos/Retag/Est/Prod/App.Retag.Est.Prod.DBI_u.pas
@@ -5,18 +5,19 @@ interface
 uses App.Ent.DBI, Sis.DBI, Sis.DBI_u, Sis.DB.DBTypes, Data.DB,
   System.Variants, Sis.Types.Integers, App.Ent.DBI_u,
   App.Retag.Est.Prod.Ent, Sis.UI.Frame.Bas.FiltroParams_u,
-  App.Retag.Est.Prod.DBI, App.Ent.Ed, App.Retag.Est.Factory;
+  App.Ent.Ed, App.Retag.Est.Factory;
 
 type
-  TProdDBI = class(TEntDBI, IProdDBI)
+  TProdDBI = class(TEntDBI)
   private
-    FProdEnt: IProdEnt;
+    function GetProdEnt: IProdEnt;
+    property ProdEnt: IProdEnt read GetProdEnt;
   protected
     function GetSqlPreencherDataSet(pValues: variant): string; override;
-    procedure SetNovaId(pIds: variant); override;
+    procedure SetNovaId(pId: variant); override;
   public
-    constructor Create(pDBConnection: IDBConnection; pEntEd: IEntEd);
-    function InsertInto: integer;
+    function Inserir(out pNovaId: variant): boolean; override;
+    function Alterar: boolean; override;
     function Ler: boolean; override;
   end;
 
@@ -27,70 +28,73 @@ uses System.SysUtils, Sis.Types.strings_u, App.Est.Types_u, Sis.Lists.Types,
 
 { TProdDBI }
 
-constructor TProdDBI.Create(pDBConnection: IDBConnection; pEntEd: IEntEd);
+function TProdDBI.Alterar: boolean;
 begin
-  inherited Create(pDBConnection);
-  FProdEnt := EntEdCastToProdEnt(pEntEd);
+
+end;
+
+function TProdDBI.GetProdEnt: IProdEnt;
+begin
+  Result := EntEdCastToProdEnt(EntEd);
 end;
 
 function TProdDBI.GetSqlPreencherDataSet(pValues: variant): string;
 begin
   Result := 'SELECT PROD_ID, DESCR, DESCR_RED, FABR_ID, FABR_NOME' +
-    //   5           6         7        8         9            10
+  // 5           6         7        8         9            10
     ', TIPO_ID, TIPO_DESCR, UNID_ID, UNID_SIGLA, ICMS_ID, ICMS_DESCR_PERC' +
 
-   //    11
+  // 11
     ', COD_BARRAS' +
 
     ', CUSTO, PRECO' +
 
     ', ATIVO, LOCALIZ, CAPAC_EMB, MARGEM' +
 
-    ' FROM PROD_PA.LISTA_GET(' + FProdEnt.LojaId.ToString + ');';
-  //SetClipboardText(Result);
+    ' FROM PROD_PA.LISTA_GET(' + ProdEnt.LojaId.ToString + ');';
+  // SetClipboardText(Result);
 end;
 
-function TProdDBI.InsertInto: integer;
+function TProdDBI.Inserir(out pNovaId: variant): boolean;
 var
   sSql: string;
   sMens: string;
-  bResultado: Boolean;
+  bResultado: boolean;
   sBarras: string;
 begin
-  Result := 0;
+  Result := False;
 
-  sBarras := FProdEnt.ProdBarrasList.GetAsString(';');
+  sBarras := ProdEnt.ProdBarrasList.GetAsString(';');
 
   sSql := 'SELECT PROD_ID FROM PROD_PA.INSERT_INTO_PROD(' +
-    QuotedStr(FProdEnt.Descr) + ',' + QuotedStr(FProdEnt.DescrRed)
+    QuotedStr(ProdEnt.Descr) + ',' + QuotedStr(ProdEnt.DescrRed)
 
-    + ',' + FProdEnt.ProdFabrEnt.Id.ToString + ',' +
-    FProdEnt.ProdTipoEnt.Id.ToString + ',' + FProdEnt.ProdUnidEnt.Id.ToString +
-    ',' + FProdEnt.ProdICMSEnt.Id.ToString
+    + ',' + ProdEnt.ProdFabrEnt.Id.ToString //
+    + ',' + ProdEnt.ProdTipoEnt.Id.ToString //
+    + ',' + ProdEnt.ProdUnidEnt.Id.ToString //
+    + ',' + ProdEnt.ProdICMSEnt.Id.ToString //
+  //
+    + ',' + ProdNatuToSql(pnatuProduto) //
+    + ',' + CurrencyToStrPonto(ProdEnt.CapacEmb) //
 
-    + ',' + ProdNatuToSql(pnatuProduto)
+    + ',' + ProdEnt.LojaId.ToString //
+    + ',' + ProdEnt.UsuarioId.ToString //
+    + ',' + ProdEnt.MachineIdentId.ToString //
 
-    + ',' + CurrencyToStrPonto(FProdEnt.CapacEmb)
-
-    + ',' + FProdEnt.LojaId.ToString + ',' + FProdEnt.UsuarioId.ToString + ',' +
-    FProdEnt.MachineIdentId.ToString
-
-    + ',' + CurrencyToStrPonto(FProdEnt.CustoNovo)
-
+    + ',' + CurrencyToStrPonto(ProdEnt.CustoNovo) //
     + ', 1' // + 1 PROD_PRECO_TABELA_ID
+    + ',' + CurrencyToStrPonto(ProdEnt.PrecoNovo) //
 
-    + ',' + CurrencyToStrPonto(FProdEnt.PrecoNovo)
+    + ',' + BooleanToStrSql(ProdEnt.Ativo) //
+    + ',' + QuotedStr(ProdEnt.Localiz) //
+    + ',' + CurrencyToStrPonto(ProdEnt.Margem) //
 
-    + ',' + BooleanToStrSql(FProdEnt.Ativo) + ',' + QuotedStr(FProdEnt.Localiz)
+    + ',' + ProdEnt.ProdBalancaEnt.BalancaTipoStr //
+    + ',' + QuotedStr(ProdEnt.ProdBalancaEnt.DptoCod) //
+    + ',' + ProdEnt.ProdBalancaEnt.ValidadeDias.ToString //
+    + ',' + QuotedStr(ProdEnt.ProdBalancaEnt.TextoEtiq) //
 
-    + ',' + CurrencyToStrPonto(FProdEnt.Margem)
-
-    + ',' + FProdEnt.ProdBalancaEnt.BalancaTipoStr + ',' +
-    QuotedStr(FProdEnt.ProdBalancaEnt.DptoCod) + ',' +
-    FProdEnt.ProdBalancaEnt.ValidadeDias.ToString + ',' +
-    QuotedStr(FProdEnt.ProdBalancaEnt.TextoEtiq)
-
-    + ',' + QuotedStr(sBarras) + ');';
+    + ',' + QuotedStr(sBarras) + ');'; //
 
 {$IFDEF DEBUG}
   SetClipboardText(sSql);
@@ -102,9 +106,10 @@ begin
     exit;
   end;
   try
-    Result := DBConnection.GetValueInteger(sSql);
+    pNovaId := DBConnection.GetValueInteger(sSql);
   finally
     DBConnection.Fechar;
+    Result := True;
   end;
 end;
 
@@ -120,30 +125,27 @@ var
 begin
   Result := False;
 
-  sSql :=
-    'SELECT ' +
-    'PROD_ID' +//0
-    ', DESCR' +//1
-    ', DESCR_RED' +//2
-    ', FABR_ID' +//3
-    ', TIPO_ID' +//4
-    ', UNID_ID' +//5
-    ', ICMS_ID' +//6
-    ', COD_BARRAS' +//7
-    ', CUSTO' +//8
-    ', PRECO' +//9
-    ', ATIVO' +//10
-    ', LOCALIZ' +//11
-    ', CAPAC_EMB' +//12
-    ', MARGEM' +//13
-    ', BAL_USO' +//14
-    ', BAL_DPTO' +//15
-    ', BAL_VALIDADE_DIAS' +//16
-    ', BAL_TEXTO_ETIQ' +//17
+  sSql := 'SELECT ' + 'PROD_ID' + // 0
+    ', DESCR' + // 1
+    ', DESCR_RED' + // 2
+    ', FABR_ID' + // 3
+    ', TIPO_ID' + // 4
+    ', UNID_ID' + // 5
+    ', ICMS_ID' + // 6
+    ', COD_BARRAS' + // 7
+    ', CUSTO' + // 8
+    ', PRECO' + // 9
+    ', ATIVO' + // 10
+    ', LOCALIZ' + // 11
+    ', CAPAC_EMB' + // 12
+    ', MARGEM' + // 13
+    ', BAL_USO' + // 14
+    ', BAL_DPTO' + // 15
+    ', BAL_VALIDADE_DIAS' + // 16
+    ', BAL_TEXTO_ETIQ' + // 17
 
-    ' FROM PROD_PA.LISTA_GET(' + FProdEnt.LojaId.ToString +')' +
-    ' WHERE PROD_ID = ' + FProdEnt.Id.ToString
-    +';';
+    ' FROM PROD_PA.LISTA_GET(' + ProdEnt.LojaId.ToString + ')' +
+    ' WHERE PROD_ID = ' + ProdEnt.Id.ToString + ';';
 
   DBConnection.Abrir;
   try
@@ -152,31 +154,31 @@ begin
     if not Result then
       exit;
 
-    FProdEnt.Descr := Q.Fields[1].AsString.Trim;
-    FProdEnt.DescrRed := Q.Fields[2].AsString.Trim;
+    ProdEnt.Descr := q.Fields[1].AsString.Trim;
+    ProdEnt.DescrRed := q.Fields[2].AsString.Trim;
 
-    FProdEnt.ProdFabrEnt.Id := Q.Fields[3].AsInteger;
-    FProdEnt.ProdTipoEnt.Id := Q.Fields[4].AsInteger;
-    FProdEnt.ProdUnidEnt.Id := Q.Fields[5].AsInteger;
-    FProdEnt.ProdICMSEnt.Id := Q.Fields[6].AsInteger;
+    ProdEnt.ProdFabrEnt.Id := q.Fields[3].AsInteger;
+    ProdEnt.ProdTipoEnt.Id := q.Fields[4].AsInteger;
+    ProdEnt.ProdUnidEnt.Id := q.Fields[5].AsInteger;
+    ProdEnt.ProdICMSEnt.Id := q.Fields[6].AsInteger;
 
-    FProdEnt.CustoAtual := Q.Fields[8].AsCurrency;
-    FProdEnt.PrecoAtual := Q.Fields[9].AsCurrency;
+    ProdEnt.CustoAtual := q.Fields[8].AsCurrency;
+    ProdEnt.PrecoAtual := q.Fields[9].AsCurrency;
 
-    FProdEnt.Ativo := Q.Fields[10].AsBoolean;
-    FProdEnt.Localiz := Q.Fields[11].AsString.Trim;
-    FProdEnt.CapacEmb := Q.Fields[12].AsCurrency;
-    FProdEnt.Margem := Q.Fields[13].AsCurrency;
+    ProdEnt.Ativo := q.Fields[10].AsBoolean;
+    ProdEnt.Localiz := q.Fields[11].AsString.Trim;
+    ProdEnt.CapacEmb := q.Fields[12].AsCurrency;
+    ProdEnt.Margem := q.Fields[13].AsCurrency;
 
-    FProdEnt.ProdBalancaEnt.BalancaTipo := TBalancaTipo(Q.Fields[14].AsInteger);
-    FProdEnt.ProdBalancaEnt.DptoCod :=  Q.Fields[15].AsString.Trim;
-    FProdEnt.ProdBalancaEnt.ValidadeDias := Q.Fields[16].AsInteger;
-    FProdEnt.ProdBalancaEnt.TextoEtiq :=  Q.Fields[17].AsString.Trim;
+    ProdEnt.ProdBalancaEnt.BalancaTipo := TBalancaTipo(q.Fields[14].AsInteger);
+    ProdEnt.ProdBalancaEnt.DptoCod := q.Fields[15].AsString.Trim;
+    ProdEnt.ProdBalancaEnt.ValidadeDias := q.Fields[16].AsInteger;
+    ProdEnt.ProdBalancaEnt.TextoEtiq := q.Fields[17].AsString.Trim;
 
-    FProdEnt.ProdBarrasList.Clear;
+    ProdEnt.ProdBarrasList.Clear;
     while not q.eof do
     begin
-      FProdEnt.ProdBarrasList.PegarBarras(Q.Fields[7].AsString.Trim, plFim);
+      ProdEnt.ProdBarrasList.PegarBarras(q.Fields[7].AsString.Trim, plFim);
       q.next;
     end;
   finally
@@ -184,10 +186,10 @@ begin
   end;
 end;
 
-procedure TProdDBI.SetNovaId(pIds: variant);
+procedure TProdDBI.SetNovaId(pId: variant);
 begin
   inherited;
-  FProdEnt.Id := VarToInteger(pIds);
+  ProdEnt.Id := VarToInteger(pId);
 end;
 
 end.
