bf7725d7260d8e0ffc337f4d3572df194b87522a
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-04-21 11:23:26 -0300

    Create ShopApp.Import_u.pas

---
+++ ShopApp/Imports/ShopApp.Import_u.pas
@@ -0,0 +1,31 @@
+unit ShopApp.Import_u;
+
+interface
+
+uses System.Classes, Sis.Types;
+
+const
+  MercadoImportSelectItems: array[1..2] of TSelectItem = (
+    (Id: 1; Descr: 'PLUBASE'),
+    (Id: 2; Descr: 'i9PDV')
+  );
+
+procedure ImportExecute;
+
+implementation
+
+procedure ImportMercadoPLUBase;
+begin
+
+end;
+
+procedure ImportMercadoi9PDV;
+begin
+
+end;
+
+procedure ImportExecute;
+begin
+end;
+
+end.
