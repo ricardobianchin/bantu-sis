96e296a463cf1bae7d787d13d5409bceca0b49e2
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-05-02 11:35:00 -0300

    IntegerList AceitaRepetidos

--- Sis/Lists/Sis.Lists.IntegerList.pas
+++ Sis/Lists/Sis.Lists.IntegerList.pas
@@ -7,7 +7,15 @@ uses Sis.Lists.Lista, System.Classes;
 type
   IIntegerList = interface(ILista)
     ['{47D2A81E-E7D8-4D30-B5AE-3C5DFD20C99E}']
+    function GetAceitaRepetidos: boolean;
+    procedure SetAceitaRepetidos(Value: boolean);
+    property AceitaRepetidos: boolean read GetAceitaRepetidos
+      write SetAceitaRepetidos;
+
     function Add(Valor: integer): integer;
+
+    function ValueToIndex(Value: integer): integer;
+
     function GetInteger(Index: integer): integer;
     property Integers[Index:integer]: integer read GetInteger; default;
   end;
