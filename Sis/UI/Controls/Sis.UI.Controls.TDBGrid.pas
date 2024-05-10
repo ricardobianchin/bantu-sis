0610d0d39c74493231758ec37a3d94d2d001c2bc
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-05-09 23:54:18 -0300

    Controls.TDBGrid CopyTextoSelecionado

--- Sis/UI/Controls/Sis.UI.Controls.TDBGrid.pas
+++ Sis/UI/Controls/Sis.UI.Controls.TDBGrid.pas
@@ -7,10 +7,12 @@ uses Vcl.DBGrids;
 function DBGridGetPrimColumnVisible(pDBGrid: TDBGrid): integer;
 procedure DBGridPosicioneColumnVisible(pDBGrid: TDBGrid);
 function DBGridColumnByFieldName(pGrid: TDBGrid; const pFieldName: String): TColumn;
+function GetTextoSelecionado(pDBGrid: TDBGrid): string;
+procedure CopyTextoSelecionado(pDBGrid: TDBGrid);
 
 implementation
 
-uses System.SysUtils;
+uses System.SysUtils, Data.DB, Sis.Win.Utils_u;
 
 function DBGridGetPrimColumnVisible(pDBGrid: TDBGrid): integer;
 var
@@ -54,6 +56,30 @@ begin
       exit;
     end;
   end;
+end;
+
+function GetTextoSelecionado(pDBGrid: TDBGrid): string;
+var
+  oField: TField;
+begin
+  Result := '';
+
+  oField := pDBGrid.SelectedField;
+  if oField = nil then
+    exit;
+
+  Result := oField.AsString;
+end;
+
+procedure CopyTextoSelecionado(pDBGrid: TDBGrid);
+var
+  sValorCelula: string;
+begin
+  sValorCelula := GetTextoSelecionado(pDBGrid);
+  if sValorCelula = '' then
+    exit;
+
+  SetClipboardText(sValorCelula);
 end;
 
 end.
