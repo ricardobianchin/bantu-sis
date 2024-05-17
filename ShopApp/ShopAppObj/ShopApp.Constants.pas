b15d778db9734405c70ae4c48551ace1f260f0d9
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-05-17 03:38:20 -0300

    ShopApp.Constants corrige atualiz_url

--- ShopApp/ShopAppObj/ShopApp.Constants.pas
+++ ShopApp/ShopAppObj/ShopApp.Constants.pas
@@ -4,7 +4,7 @@ interface
 
 const
   ATUALIZ_ARQ_SUBPASTA = 'Inst\Update\InstUpdate\MercadoAtualiz.exe';
-  ATUALIZ_URL = 'https://www.bianch.in/arqs/daros/MercadoAtualiz.exe';
+  ATUALIZ_URL = 'https://www.bianch.in/cli/daros/MercadoAtualiz.exe';
 
 implementation
 
