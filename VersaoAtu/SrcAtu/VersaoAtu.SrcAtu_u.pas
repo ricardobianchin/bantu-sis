9c74db80ea8f6c7d43cac7fb7f590fa2154455e3
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-05-19 19:38:52 -0300

    VersaoAtu ok

--- VersaoAtu/SrcAtu/VersaoAtu.SrcAtu_u.pas
+++ VersaoAtu/SrcAtu/VersaoAtu.SrcAtu_u.pas
@@ -2,24 +2,48 @@ unit VersaoAtu.SrcAtu_u;
 
 interface
 
-procedure SrcAtuExecute(pDescribe, pHash, pGMTDtH: string);
+procedure SrcAtuExecute(pDescribe, pGMTDtH, pHash: string);
 
 implementation
 
-uses System.Classes;
+uses System.Classes, System.SysUtils;
 
 const
+  VERSAO_SRC_FILENAME = 'C:\Pr\app\bantu\bantu-sis\Src\App\AppVersao\App.Versao_u.pas';
   MARCA_VERSAO = '{versaoatu versao}';
   MARCA_DTH = '{versaoatu datahora}';
   MARCA_HASH = '{versaoatu Hash}';
-  VERSAO_SRC_FILENAME = 'C:\Pr\app\bantu\bantu-sis\Src\App\AppVersao\App.Versao_u.pas';
 
-procedure SrcAtuExecute(pDescribe, pHash, pGMTDtH: string);
+procedure SrcAtuExecute(pDescribe, pGMTDtH, pHash: string);
 var
   sl: TStringList;
+  I: Integer;
+  sLinha: string;
 begin
   sl := TStringList.Create;
   try
+    sl.LoadFromFile(VERSAO_SRC_FILENAME);
+    for I := 0 to sl.Count - 1  do
+    begin
+      sLinha := sl[i];
+      if Pos(MARCA_VERSAO, sLinha) > 0 then
+      begin
+        sLinha := '    ' + QuotedStr(pDescribe) + MARCA_VERSAO;
+        sl[i] := sLinha;
+      end
+      else if Pos(MARCA_DTH, sLinha) > 0 then
+      begin
+        sLinha := '    ' + QuotedStr(pGMTDtH) + MARCA_DTH;
+        sl[i] := sLinha;
+      end
+      else if Pos(MARCA_HASH, sLinha) > 0 then
+      begin
+        sLinha := '    ' + QuotedStr(pHash) + MARCA_HASH;
+        sl[i] := sLinha;
+      end;
+
+      sl.savetofile(VERSAO_SRC_FILENAME);
+    end;
   finally
     sl.Free;
   end;
--- VersaoAtu/VersaoAtu.dpr
+++ VersaoAtu/VersaoAtu.dpr
@@ -1,24 +1,31 @@
 program VersaoAtu;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
   System.SysUtils,
   VersaoAtu.SrcAtu_u in 'SrcAtu\VersaoAtu.SrcAtu_u.pas';
+
 var
   sDescribe: string;
-  sHash: string;
   sGMTDtH: string;
+  sHash: string;
+
+procedure LeiaStdIn;
+begin
+  ReadLn(sDescribe);
+  ReadLn(sGMTDtH);
+  ReadLn(sHash);
+end;
+
 begin
   try
-    ReadLn(sDescribe);
-    ReadLn(sHash);
-    ReadLn(sGMTDtH);
-    SrcAtuExecute(sDescribe, sHash, sGMTDtH);
+    LeiaStdIn;
+    SrcAtuExecute(sDescribe, sGMTDtH, sHash);
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
   end;
+
 end.
