8eae6eee09240e3697a6f64d48c417e4d1f11b8e
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-05-19 19:07:38 -0300

    Sis.Win.Factory cria  procedure WinExecuteComando

--- Sis/Win/Sis.Win.Factory.pas
+++ Sis/Win/Sis.Win.Factory.pas
@@ -2,33 +2,40 @@ unit Sis.Win.Factory;
 
 interface
 
-uses sis.win.VersionInfo, sis.ui.io.output, sis.win.execute, WinApi.Windows, sis.ui.io.factory;
+uses Sis.Win.VersionInfo, Sis.ui.io.output, Sis.Win.execute, WinApi.Windows,
+  Sis.ui.io.Factory;
 
 function WinVersionCreate: IWinVersionInfo;
-function WinExecuteCreate(
-      pExecuteFile, pParams, pStartIn: string;
-      pExecuteAoCriar: boolean;
-      pShowMode: integer = SW_SHOWNORMAL;
-      pOutput: IOutput=nil
-      ): IWinExecute;
+function WinExecuteCreate(pExecuteFile, pParams, pStartIn: string;
+  pExecuteAoCriar: boolean; pShowMode: integer = SW_SHOWMINNOACTIVE;
+  pOutput: IOutput = nil): IWinExecute;
+procedure WinExecuteComando(pExecuteFile, pParams: string; pStartIn: string = '';
+  pOutput: IOutput = nil);
 
 implementation
 
-uses Sis.Win.VersionInfo_u, Sis.UI.IO.Output.Mudo_u, Sis.Win.Execute_u;
+uses Sis.Win.VersionInfo_u, Sis.ui.io.output.Mudo_u, Sis.Win.Execute_u,
+  Sis.ui.io.Files.Factory;
 
 function WinVersionCreate: IWinVersionInfo;
 begin
   Result := TWinVersionInfo.Create;
 end;
 
-function WinExecuteCreate(
-      pExecuteFile, pParams, pStartIn: string;
-      pExecuteAoCriar: boolean;
-      pShowMode: integer;
-      pOutput: IOutput
-      ): IWinExecute;
+function WinExecuteCreate(pExecuteFile, pParams, pStartIn: string;
+  pExecuteAoCriar: boolean; pShowMode: integer; pOutput: IOutput): IWinExecute;
 begin
-  result := TWinExecute.Create(pExecuteFile, pParams, pStartIn, pExecuteAoCriar, pShowMode, pOutput);
+  Result := TWinExecute.Create(pExecuteFile, pParams, pStartIn, pExecuteAoCriar,
+    pShowMode, pOutput);
+end;
+
+procedure WinExecuteComando(pExecuteFile, pParams: string; pStartIn: string;
+  pOutput: IOutput);
+var
+  we: IWinExecute;
+begin
+  we := WinExecuteCreate(pExecuteFile, pParams, pStartIn, true);
+  we.EspereExecucao(pOutput);
 end;
 
 end.
