ab937d75db62b6d06598b880790cadceb484533e
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-05-19 19:06:18 -0300

    app cria app.versao

---
+++ App/AppVersao/App.Versao_u.pas
@@ -0,0 +1,20 @@
+unit App.Versao_u;
+
+interface
+
+const
+  Versao = //
+    ''{versaoatu versao}
+    ; //
+
+  DataHora = //
+    ''{versaoatu datahora}
+    ; //
+
+  Hash = //
+    ''{versaoatu Hash}
+    ; //
+
+implementation
+
+end.
