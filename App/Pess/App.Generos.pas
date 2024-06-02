b0a7e82cf4082823dfc58375eab2023fccf929d3
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-06-02 11:48:39 -0300

    Generos cria ent

--- App/Pess/App.Generos.pas
+++ App/Pess/App.Generos.pas
@@ -7,8 +7,8 @@ uses System.Classes;
 type
   IGeneros = interface(IInterface)
     ['{2D324CC2-7BDF-431E-A503-24472FF6EC20}']
-    function GetGenerosSL: TStrings;
-    property GenerosSL: TStrings read GetGenerosSL;
+    function GetValoresSL: TStrings;
+    property ValoresSL: TStrings read GetValoresSL;
   end;
 
 implementation
