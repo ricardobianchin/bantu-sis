c099abc9a7794ab6e375df92a6d4bd02c88769ac
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-06-07 15:11:38 -0300

    Form.DataSet.Pess doatualizar

--- App/Pess/App.UI.Form.DataSet.Pess_u.pas
+++ App/Pess/App.UI.Form.DataSet.Pess_u.pas
@@ -13,6 +13,20 @@ type
     procedure ShowTimer_BasFormTimer(Sender: TObject);
   private
     { Private declarations }
+  protected
+    { Protected declarations }
+    procedure DoAtualizar(Sender: TObject); override;
+    function DoInserir: boolean; override;
+    procedure DoLer; override;
+    procedure DoAlterar; override;
+
+    function GetNomeArqTabView: string; override;
+    procedure ToolBar1CrieBotoes; override;
+    procedure RecordToEnt; override;
+    procedure EntToRecord; override;
+
+    procedure LeRegEInsere(q: TDataSet; pRecNo: integer); override;
+
   public
     { Public declarations }
   end;
@@ -24,12 +38,77 @@ implementation
 
 {$R *.dfm}
 
-uses Sis.UI.Controls.Utils;
+uses Sis.UI.Controls.Utils, Sis.UI.Controls.TDBGrid;
+
+procedure TAppPessDataSetForm.DoAlterar;
+begin
+  inherited;
+
+end;
+
+procedure TAppPessDataSetForm.DoAtualizar(Sender: TObject);
+var
+  Resultado: boolean;
+begin
+  FDMemTable.DisableControls;
+  FDMemTable.BeginBatch;
+  FDMemTable.EmptyDataSet;
+
+  try
+    EntDBI.PreencherDataSet(0, LeRegEInsere);
+
+  finally
+    FDMemTable.First;
+    FDMemTable.EndBatch;
+    FDMemTable.EnableControls;
+    DBGridPosicioneColumnVisible(DBGrid1);
+  end;
+end;
+
+function TAppPessDataSetForm.DoInserir: boolean;
+begin
+
+end;
+
+procedure TAppPessDataSetForm.DoLer;
+begin
+  inherited;
+
+end;
+
+procedure TAppPessDataSetForm.EntToRecord;
+begin
+  inherited;
+
+end;
+
+function TAppPessDataSetForm.GetNomeArqTabView: string;
+begin
+
+end;
+
+procedure TAppPessDataSetForm.LeRegEInsere(q: TDataSet; pRecNo: integer);
+begin
+  inherited;
+
+end;
+
+procedure TAppPessDataSetForm.RecordToEnt;
+begin
+  inherited;
+
+end;
 
 procedure TAppPessDataSetForm.ShowTimer_BasFormTimer(Sender: TObject);
 begin
   inherited;
   ClearStyleElements(Self);
+end;
+
+procedure TAppPessDataSetForm.ToolBar1CrieBotoes;
+begin
+  inherited;
+
 end;
 
 end.
