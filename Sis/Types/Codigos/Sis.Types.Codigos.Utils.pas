0cf5fbeff6d449ecc33c99dda79c5cc9f0a0dedf
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-07-10 11:36:25 -0300

    sis types codigos cpf random

--- Sis/Types/Codigos/Sis.Types.Codigos.Utils.pas
+++ Sis/Types/Codigos/Sis.Types.Codigos.Utils.pas
@@ -21,9 +21,11 @@ function EAN8Dig(pCod: string): char;
 function EAN13Dig(pCod: string): char;
 function EAN14Dig(pCod: string): char;
 function CNPJDig(pCod: string): string;
+function CPFDig(pCod: string): string;
 
 function EAN13GetRandom: string;
 function CNPJGetRandom: string;
+function CPFGetRandom: string;
 
 function IdToEan13(pId: integer): string;
 
@@ -633,6 +635,29 @@ begin
   Result := Result + CNPJDig(Result);
 end;
 
+function CPFGetRandom: string;
+const
+  Digitos: array [0 .. 9] of char = ('0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9');
+  LEN_CODIGO = 11;
+  PREFIXO = '000';
+  LEN_DESEJADO = LEN_CODIGO - 2 { dig } - Length(PREFIXO);
+
+var
+  cAtual: char;
+  i, ic: integer;
+begin
+  Result := PREFIXO;
+  for i := 1 to LEN_DESEJADO do
+  begin
+    ic := Random(Length(Digitos));
+    cAtual := Digitos[ic];
+    Result := Result + cAtual
+  end;
+
+  Result := Result + CPFDig(Result);
+end;
+
 function CNPJDig(pCod: string): string;
 var
   L, soma, ind, i, resto, dv1, dv2: integer;
@@ -671,6 +696,38 @@ begin
       ind := 9;
   end;
   resto := soma mod 11;
+  if (resto = 0) or (resto = 1) then
+    dv2 := 0
+  else
+    dv2 := 11 - resto;
+
+  result := Chr(Ord('0') + dv1) + Chr(Ord('0') + dv2);
+end;
+
+function CPFDig(pCod: string): string;
+var
+  soma, p, resto, dv1, dv2: integer;
+begin
+//50419560068
+  soma := 0;
+  for p := 1 to 9 do
+    inc(soma, strtoint(pCod[p]) * (11 - p));
+
+  resto := soma mod 11;
+
+  if (resto = 0) or (resto = 1) then
+    dv1 := 0
+  else
+    dv1 := 11 - resto;
+
+  pCod := pCod + Chr(Ord('0') + dv1);
+
+  soma := 0;
+  for p := 1 to 10 do
+    inc(soma, strtoint(pCod[p]) * (12 - p));
+
+  resto := soma mod 11;
+
   if (resto = 0) or (resto = 1) then
     dv2 := 0
   else
