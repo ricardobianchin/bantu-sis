8c38f050ed63008d4b32a46bb3f482347645489c
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-07-13 02:48:57 -0300

    app pess utils ajusta ENDER_TABVIEW_ORDEM_INDEX

--- App/Pess/App.Pess.Utils.pas
+++ App/Pess/App.Pess.Utils.pas
@@ -3,7 +3,7 @@ unit App.Pess.Utils;
 interface
 
 const
-  ENDER_TABVIEW_ORDEM_INDEX = 15;
+  ENDER_TABVIEW_ORDEM_INDEX = 16;
 
 type
   TEnderQuantidadePermitida = (endqtdNenhum, endqtdUm, endqtdVarios);
