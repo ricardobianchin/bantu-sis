7ebe83b32eade6885a7b206abc6f8ee4ce230b81
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-08-16 11:27:18 -0300

    dataset perfil de uso cria EntToRecord

--- App/Pess/PerfilDeUso/App.UI.Form.DataSet.Acesso.PerfilDeUso_u.pas
+++ App/Pess/PerfilDeUso/App.UI.Form.DataSet.Acesso.PerfilDeUso_u.pas
@@ -9,10 +9,11 @@ uses
   Vcl.DBGrids, Vcl.ToolWin, App.Acesso.PerfilDeUso.Ent.Factory_u,
   App.Acesso.PerfilDeUso.Ent, App.AppInfo, Sis.Config.SisConfig, Sis.Usuario,
   Sis.DB.DBTypes, Sis.UI.IO.Output, Sis.UI.IO.Output.ProcessLog, App.Ent.Ed,
-  App.Ent.DBI, App.UI.TabSheet.DataSet.Types_u;
+  App.Ent.DBI, App.UI.TabSheet.DataSet.Types_u, FireDAC.Comp.Client;
 
 type
   TPerfilDeUsoDataSetForm = class(TTabSheetDataSetBasForm)
+    procedure ShowTimer_BasFormTimer(Sender: TObject);
   private
     { Private declarations }
     FPerfilDeUsoEnt: IPerfilDeUsoEnt;
@@ -24,7 +25,9 @@ type
     procedure DoAlterar; override;
 
     procedure ToolBar1CrieBotoes; override;
+
     procedure RecordToEnt; override;
+    procedure EntToRecord; override;
   public
     { Public declarations }
     constructor Create(AOwner: TComponent; pFormClassNamesSL: TStringList;
@@ -58,9 +61,17 @@ begin
 end;
 
 procedure TPerfilDeUsoDataSetForm.DoAlterar;
+var
+  Resultado: boolean;
 begin
-  inherited;
+  Resultado := PerfilDeUsoPerg(Self, AppInfo, EntEd, EntDBI);
 
+  if not Resultado then
+    exit;
+
+  FDMemTable.Append;
+  EntToRecord;
+  FDMemTable.Post;
 end;
 
 procedure TPerfilDeUsoDataSetForm.DoAtualizar(Sender: TObject);
@@ -81,16 +92,16 @@ begin
 end;
 
 function TPerfilDeUsoDataSetForm.DoInserir: boolean;
-var
-  oDBConnectionParams: TDBConnectionParams;
-  oDBConnection: IDBConnection;
+//var
+//  oDBConnectionParams: TDBConnectionParams;
+//  oDBConnection: IDBConnection;
 begin
   inherited;
-  oDBConnectionParams := LocalDoDBToDBConnectionParams(TLocalDoDB.ldbServidor,
-    AppInfo, SisConfig);
+//  oDBConnectionParams := LocalDoDBToDBConnectionParams(TLocalDoDB.ldbServidor,
+//    AppInfo, SisConfig);
 
-  oDBConnection := DBConnectionCreate('Retag.PerfilDeUso.Ed.Ins.Conn', SisConfig, DBMS,
-    oDBConnectionParams, ProcessLog, Output);
+//  oDBConnection := DBConnectionCreate('Retag.PerfilDeUso.Ed.Ins.Conn', SisConfig, DBMS,
+//    oDBConnectionParams, ProcessLog, Output);
 
   Result := PerfilDeUsoPerg(Self, AppInfo, EntEd, EntDBI);
 
@@ -100,6 +111,18 @@ begin
   FDMemTable.Append;
   EntToRecord;
   FDMemTable.Post;
+end;
+
+procedure TPerfilDeUsoDataSetForm.EntToRecord;
+var
+  Tab: TFDMemTable;
+begin
+  inherited;
+  Tab := FDMemTable;
+
+  Tab.Fields[0{perfil_de_uso_id}].AsInteger := FPerfilDeUsoEnt.Id;
+  Tab.Fields[1{nome}].AsString := FPerfilDeUsoEnt.Descr;
+  Tab.Fields[2{de_sistema}].AsBoolean := FPerfilDeUsoEnt.DeSistema;
 end;
 
 function TPerfilDeUsoDataSetForm.GetNomeArqTabView: string;
@@ -112,9 +135,21 @@ begin
 end;
 
 procedure TPerfilDeUsoDataSetForm.RecordToEnt;
+var
+  Tab: TFDMemTable;
 begin
   inherited;
+  Tab := FDMemTable;
 
+  FPerfilDeUsoEnt.Id := Tab.Fields[0{perfil_de_uso_id}].AsInteger;
+  FPerfilDeUsoEnt.Descr := Trim(Tab.Fields[1{nome}].AsString);
+  FPerfilDeUsoEnt.DeSistema := Tab.Fields[2{de_sistema}].AsBoolean;
+end;
+
+procedure TPerfilDeUsoDataSetForm.ShowTimer_BasFormTimer(Sender: TObject);
+begin
+  inherited;
+  InsAction_DatasetTabSheet.Execute;
 end;
 
 procedure TPerfilDeUsoDataSetForm.ToolBar1CrieBotoes;
