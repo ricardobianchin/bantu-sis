804495c874cde8210ab85bd8144243f39995790b
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-08-23 15:17:18 -0300

    ft-func ShopApp.Constanst ajusa URL

--- ShopApp/ShopAppObj/ShopApp.Constants.pas
+++ ShopApp/ShopAppObj/ShopApp.Constants.pas
@@ -4,7 +4,7 @@ interface
 
 const
   ATUALIZ_ARQ_SUBPASTA = 'Inst\Update\InstUpdate\MercadoAtualiz.exe';
-  ATUALIZ_URL = 'https://www.bianch.in/cli/daros/MercadoAtualiz.exe';
+  ATUALIZ_URL = 'https://www.bianch.in/downloads/bantu/app/shop/MercadoAtualiz.exe';
 
 implementation
 
