a5636bb3205633b1e9378b5f5eaa14fe78aa24ad
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-08-30 08:38:51 -0300

    pess func ent_u cria dados usuario

--- App/Pess/Funcionario/App.Pess.Funcionario.Ent_u.pas
+++ App/Pess/Funcionario/App.Pess.Funcionario.Ent_u.pas
@@ -7,11 +7,28 @@ uses App.Pess.Funcionario.Ent, App.Pess.Ent_u, App.PessEnder.List, App.Pess.Util
 type
   TPessFuncionarioEnt = class(TPessEnt, IPessFuncionarioEnt)
   private
+    FNomeDeUsuario: string;
+    FSenha: string;
+    FCryVer: smallint;
+
+    function GetNomeDeUsuario: string;
+    procedure SetNomeDeUsuario(const Value: string);
+
+    function GetSenha: string;
+    procedure SetSenha(const Value: string);
+
+    function GetCryVer: smallint;
+    procedure SetCryVer(const Value: smallint);
+
   protected
     function GetNomeEnt: string; override;
     function GetNomeEntAbrev: string; override;
     function GetTitulo: string; override;
   public
+    property NomeDeUsuario: string read GetNomeDeUsuario write SetNomeDeUsuario;
+    property Senha: string read GetSenha write SetSenha;
+    property CryVer: smallint read GetCryVer write SetCryVer;
+
     procedure LimparEnt; override;
   end;
 
@@ -20,6 +37,16 @@ implementation
 uses Data.DB;
 
 { TPessFuncionarioEnt }
+
+function TPessFuncionarioEnt.GetCryVer: smallint;
+begin
+  Result := FCryVer;
+end;
+
+function TPessFuncionarioEnt.GetNomeDeUsuario: string;
+begin
+  Result := FNomeDeUsuario;
+end;
 
 function TPessFuncionarioEnt.GetNomeEnt: string;
 begin
@@ -31,6 +58,11 @@ begin
   Result := 'Func';
 end;
 
+function TPessFuncionarioEnt.GetSenha: string;
+begin
+  Result := FSenha;
+end;
+
 function TPessFuncionarioEnt.GetTitulo: string;
 begin
   Result := 'Funcionários';
@@ -40,6 +72,21 @@ procedure TPessFuncionarioEnt.LimparEnt;
 begin
   inherited;
 
+end;
+
+procedure TPessFuncionarioEnt.SetCryVer(const Value: smallint);
+begin
+  FCryVer := Value;
+end;
+
+procedure TPessFuncionarioEnt.SetNomeDeUsuario(const Value: string);
+begin
+  FNomeDeUsuario := Value;
+end;
+
+procedure TPessFuncionarioEnt.SetSenha(const Value: string);
+begin
+  FSenha := Value;
 end;
 
 end.
