59615f1590d944786f3c173cdf77f4cd01dc0f15
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-08-31 10:57:24 -0300

    app pess cliente ent cria

---
+++ App/Pess/Cliente/App.Pess.Cliente.Ent.pas
@@ -0,0 +1,14 @@
+unit App.Pess.Cliente.Ent;
+
+interface
+
+uses App.Pess.Ent;
+
+type
+  IPessClienteEnt = interface(IPessEnt)
+    ['{709F97FD-6A2C-4F54-A695-4ADD348DEE83}']
+  end;
+
+implementation
+
+end.
