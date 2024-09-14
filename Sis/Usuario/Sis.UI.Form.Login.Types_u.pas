f7ca72c3ed070ff2676e826560fb5cf65135c2a9
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-09-14 11:16:51 -0300

    sis login types exclui LoginPergModoToStr

--- Sis/Usuario/Sis.UI.Form.Login.Types_u.pas
+++ Sis/Usuario/Sis.UI.Form.Login.Types_u.pas
@@ -5,17 +5,6 @@ interface
 type
   TLoginPergModo = (ltLogando, ltMudandoSenha, ltCriandoSenha);
 
-function LoginPergModoToStr(pLoginPergModo: TLoginPergModo): string;
-
 implementation
-
-function LoginPergModoToStr(pLoginPergModo: TLoginPergModo): string;
-begin
-  case pLoginPergModo of
-    ltLogando: Result := 'Logando';
-    ltMudandoSenha: Result := 'Mudando Senha';
-    ltCriandoSenha: Result := 'Criando Senha';
-  end;
-end;
 
 end.
