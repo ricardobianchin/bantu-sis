95550e70a04af58616cd825b54c72b289b512a35
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-09-19 23:04:46 -0300

    SIS Diag.Btn.CheckListView GetIdsSelecionadas

--- Sis/UI/BasForm/Sis.UI.Form.Bas.Diag.Btn.CheckListView_u.pas
+++ Sis/UI/BasForm/Sis.UI.Form.Bas.Diag.Btn.CheckListView_u.pas
@@ -14,9 +14,11 @@ type
     TituloLabel: TLabel;
   private
     { Private declarations }
+    function GetIdsSelecionadasAsStringCSV: string;
   protected
     procedure PreencherCheckListBox; virtual;
     procedure AjusteControles; override;
+    property IdsSelecionadasAsStringCSV: string read GetIdsSelecionadasAsStringCSV;
   public
     { Public declarations }
     constructor Create(AOwner: TComponent; pCaption: TCaption;
@@ -49,6 +51,28 @@ begin
   TituloLabel.Caption := pTitulo;
   CheckListBox1.Align := alClient;
   Height := GetToolFormHeight;
+end;
+
+function TCheckListViewDiagBasForm.GetIdsSelecionadasAsStringCSV: string;
+var
+  PPointerAtual: Pointer;
+  iIdAtual: integer;
+  bTem: boolean;
+  i: integer;
+begin
+  Result := '';
+  for I := 0 to CheckListBox1.Items.Count - 1 do
+  begin
+    bTem := CheckListBox1.Checked[i];
+    if not bTem then
+      Continue;
+
+    PPointerAtual := CheckListBox1.Items.Objects[i];
+    iIdAtual := Integer(PPointerAtual);
+    if Result <> '' then
+      Result := Result + ';';
+    Result := Result + iIdAtual.ToString;
+  end;
 end;
 
 procedure TCheckListViewDiagBasForm.PreencherCheckListBox;
