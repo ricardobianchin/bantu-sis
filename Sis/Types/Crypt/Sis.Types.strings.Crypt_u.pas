8ea61c8daceb47383c4ee860d11d1ebcd8d1827f
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-09-23 16:05:05 -0300

    Update Sis.Types.strings.Crypt_u.pas

--- Sis/Types/Crypt/Sis.Types.strings.Crypt_u.pas
+++ Sis/Types/Crypt/Sis.Types.strings.Crypt_u.pas
@@ -2,25 +2,25 @@ unit Sis.Types.strings.Crypt_u;
 
 interface
 
-procedure Encriptar(pCryVer: integer; pStr: string; out pEncriptado: string);
-procedure Desencriptar(pCryVer: integer; pEncriptado: string; out pStr: string);
+procedure Encriptar(pCryVer: integer; pStrDesencriptada: string; out pStrEncriptada: string);
+procedure Desencriptar(pCryVer: integer; pStrEncriptada: string; out pStrDesencriptada: string);
 
 implementation
 
 uses Sis.Types.strings.Crypt1_u;
 
 // Encripta uma string usando os primos como fatores
-procedure Encriptar(pCryVer: integer; pStr: string; out pEncriptado: string);
+procedure Encriptar(pCryVer: integer; pStrDesencriptada: string; out pStrEncriptada: string);
 begin
   case pCryVer of
-    1: Encriptar1(pStr, pEncriptado);
+    1: Encriptar1(pStrDesencriptada, pStrEncriptada);
   end;
 end;
 
-procedure Desencriptar(pCryVer: integer; pEncriptado: string; out pStr: string);
+procedure Desencriptar(pCryVer: integer; pStrEncriptada: string; out pStrDesencriptada: string);
 begin
   case pCryVer of
-    1: Desencriptar1(pEncriptado, pStr);
+    1: Desencriptar1(pStrEncriptada, pStrDesencriptada);
   end;
 end;
 
