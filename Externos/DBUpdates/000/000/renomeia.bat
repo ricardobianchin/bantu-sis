798d89b7f5c9869747414b4e3b986613509b83e4
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-09-27 10:42:26 -0300

    Update renomeia.bat

--- Externos/DBUpdates/000/000/renomeia.bat
+++ Externos/DBUpdates/000/000/renomeia.bat
@@ -70,3 +70,4 @@ copy "C:\Pr\app\bantu\bantu-sis\Src\Externos\DBUpdates\000\000\dbupdate 00000000
 copy "C:\Pr\app\bantu\bantu-sis\Src\Externos\DBUpdates\000\000\dbupdate 000000002.txt" "C:\Pr\app\bantu\bantu-sis\Src\Externos\DBUpdates\000\000\dbupdate 000000003.txt"
 copy "C:\Pr\app\bantu\bantu-sis\Src\Externos\DBUpdates\000\000\dbupdate 000000001.txt" "C:\Pr\app\bantu\bantu-sis\Src\Externos\DBUpdates\000\000\dbupdate 000000002.txt"
 
+pause
