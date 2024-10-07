ed074d37b5662dc48e0d0da0b2da742426c6c9cf
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-10-07 18:12:02 -0300

    sis entities types cria TTerminalIdHelper

--- Sis/Entities/Sis.Entities.Types.pas
+++ Sis/Entities/Sis.Entities.Types.pas
@@ -3,9 +3,29 @@ unit Sis.Entities.Types;
 interface
 
 type
-  TLojaId = smallint;
-  TTerminalId = smallint;
+  TLojaId = type Smallint;
+  TTerminalId = type SmallInt;
+
+  TTerminalIdHelper = record helper for TTerminalId
+    function ToString: string;
+    function ToStrZero: string;
+  end;
 
 implementation
 
+uses System.SysUtils;
+
+{ TTerminalIdHelper }
+
+function TTerminalIdHelper.ToString: string;
+begin
+  Result := IntToStr(Self);
+end;
+
+function TTerminalIdHelper.ToStrZero: string;
+begin
+  Result := Format('%.3d', [Self]);
+end;
+
 end.
+
