76f5aace3969f9414220d38c597b94cee92d8e83
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-10-08 09:24:04 -0300

    Update App.Testes.Config.ModuConf.Ambi.Loja.pas
    - Boolean ini uppercase

--- App/AppObj/TestesConfig/ModuConf/Ambi/Loja/App.Testes.Config.ModuConf.Ambi.Loja.pas
+++ App/AppObj/TestesConfig/ModuConf/Ambi/Loja/App.Testes.Config.ModuConf.Ambi.Loja.pas
@@ -5,9 +5,9 @@ interface
 type
   ITesteConfigModuConfAmbiLoja = interface(IInterface)
     ['{C0C35B10-A0B2-4CC5-8971-D4A548F35CD0}']
-    function GetAutoExec: boolean;
-    procedure SetAutoExec(Value: boolean);
-    property AutoExec: boolean read GetAutoExec write SetAutoExec;
+    function GetAutoExec: Boolean;
+    procedure SetAutoExec(Value: Boolean);
+    property AutoExec: Boolean read GetAutoExec write SetAutoExec;
   end;
 
 implementation
