76ebe833ef42be0d6fae0f75822d6d7f26c82149
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-10-09 10:04:28 -0300

    app term carga frame cria controles

--- App/DB/Term/Carga/App.DB.Term.Carga.Frame_u.dfm
+++ App/DB/Term/Carga/App.DB.Term.Carga.Frame_u.dfm
@@ -1,11 +1,44 @@
 inherited TermCargaFrameFrame: TTermCargaFrameFrame
-  object BitBtn1: TBitBtn
-    Left = 16
-    Top = 16
-    Width = 75
+  object AtualizarListaBitBtn: TBitBtn
+    Left = 8
+    Top = 146
+    Width = 92
     Height = 25
-    Caption = 'BitBtn1'
+    Action = AtualizarListaAction
+    Anchors = [akLeft, akBottom]
+    Caption = 'Atualizar Lista'
     TabOrder = 0
-    OnClick = BitBtn1Click
+  end
+  object TermCheckListBox: TCheckListBox
+    Left = 0
+    Top = 0
+    Width = 385
+    Height = 125
+    Align = alTop
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    ItemHeight = 15
+    TabOrder = 1
+    ExplicitTop = 15
+  end
+  object TermCargaBitBtn: TBitBtn
+    Left = 112
+    Top = 146
+    Width = 92
+    Height = 25
+    Action = TermCargaAction
+    Anchors = [akLeft, akBottom]
+    Caption = 'Enviar Dados'
+    TabOrder = 2
+  end
+  object ActionList1: TActionList
+    Left = 128
+    Top = 40
+    object AtualizarListaAction: TAction
+      Caption = 'Atualizar Lista'
+      OnExecute = AtualizarListaActionExecute
+    end
+    object TermCargaAction: TAction
+      Caption = 'Enviar Dados'
+    end
   end
 end
--- App/DB/Term/Carga/App.DB.Term.Carga.Frame_u.pas
+++ App/DB/Term/Carga/App.DB.Term.Carga.Frame_u.pas
@@ -3,21 +3,32 @@ unit App.DB.Term.Carga.Frame_u;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Sis.Entities.TerminalList, Sis.Config.SisConfig,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Sis.UI.Frame.Bas_u, Vcl.StdCtrls,
-  Vcl.Buttons, App.AppObj;
+  Vcl.Buttons, App.AppInfo, Sis.DB.DBTypes, System.Actions, Vcl.ActnList,
+  Vcl.CheckLst;
 
 type
   TTermCargaFrameFrame = class(TBasFrame)
-    BitBtn1: TBitBtn;
-    procedure BitBtn1Click(Sender: TObject);
+    AtualizarListaBitBtn: TBitBtn;
+    ActionList1: TActionList;
+    AtualizarListaAction: TAction;
+    TermCheckListBox: TCheckListBox;
+    TermCargaAction: TAction;
+    TermCargaBitBtn: TBitBtn;
+    procedure AtualizarListaActionExecute(Sender: TObject);
   private
     { Private declarations }
-    FAppObj: IAppObj;
-    procedure PreenchaConectionList;
+    FAppInfo: IAppInfo;
+    FSisConfig: ISisConfig;
+    FServerDBConnection: IDBConnection;
+    FTerminalList: ITerminalList;
+    procedure PreencherTermListBox;
   public
     { Public declarations }
-    constructor Create(AOwner: TComponent; pAppObj: IAppObj); reintroduce; virtual;
+    constructor Create(AOwner: TComponent; pAppInfo: IAppInfo; pSisConfig: ISisConfig);
+      reintroduce; virtual;
   end;
 
 var
@@ -27,22 +38,46 @@ implementation
 
 {$R *.dfm}
 
-procedure TTermCargaFrameFrame.BitBtn1Click(Sender: TObject);
+uses App.DB.Utils, Sis.Sis.Constants, Sis.DB.Factory, Sis.Entities.Terminal,
+  Sis.Entities.Factory;
+
+procedure TTermCargaFrameFrame.AtualizarListaActionExecute(Sender: TObject);
 begin
   inherited;
-showmessage('a');
+  PreencherTermListBox;
 end;
 
-constructor TTermCargaFrameFrame.Create(AOwner: TComponent; pAppObj: IAppObj);
+constructor TTermCargaFrameFrame.Create(AOwner: TComponent; pAppInfo: IAppInfo; pSisConfig: ISisConfig);
 begin
   inherited Create(AOwner);
-  FAppObj := pAppObj;
-  PreenchaConectionList;
+  FAppInfo := pAppInfo;
+  FSisConfig := pSisConfig;
+  FTerminalList := TerminalListCreate;
 end;
 
-procedure TTermCargaFrameFrame.PreenchaConectionList;
+procedure TTermCargaFrameFrame.PreencherTermListBox;
+var
+  oDBConnectionParams: TDBConnectionParams;
+  oDBConnection: IDBConnection;
+  I: Integer;
+  oTerminal: ITerminal;
+  sText: string;
 begin
+  oDBConnectionParams := TerminalIdToDBConnectionParams(TERMINAL_ID_RETAGUARDA,
+    FAppInfo, FSisConfig);
 
+  oDBConnection := DBConnectionCreate('CargaFrame.PreencherList.Conn',
+    FSisConfig, oDBConnectionParams, nil, nil);
+
+  PreencherTerminalList(oDBConnection, FAppInfo, FTerminalList);
+
+  TermCheckListBox.Items.Clear;
+  for I := 0 to FTerminalList.Count - 1 do
+  begin
+    oTerminal := FTerminalList[I];
+    sText := oTerminal.AsText;
+    TermCheckListBox.Items.Add(sText);
+  end;
 end;
 
 end.
