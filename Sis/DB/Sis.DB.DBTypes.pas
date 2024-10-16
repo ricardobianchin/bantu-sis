680f50afd1282c3a6f010552035fa910e6f7daad
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-10-16 14:19:41 -0300

    Update Sis.DB.DBTypes.pas
    cria SetParamCurrency

--- Sis/DB/Sis.DB.DBTypes.pas
+++ Sis/DB/Sis.DB.DBTypes.pas
@@ -31,6 +31,7 @@ type
   TFirebirdVersion = TDBVersion;
 
 procedure SetParamDateTime(pFDParam: TFDParam; pDt: TDateTime);
+procedure SetParamCurrency(pFDParam: TFDParam; pCurr: Currency);
 
 const
   DBFrameworkNames: array [TDBFramework] of string = ('NAOINDICADO', 'FIREDAC');
@@ -230,6 +231,64 @@ begin
       pFDParam.AsDateTIme := pDt;
     else //ftTimeStampOffset:
       pFDParam.Value := pDt;
+  end;
+end;
+
+procedure SetParamCurrency(pFDParam: TFDParam; pCurr: Currency);
+begin
+  case pFDParam.DataType of
+    ftUnknown: ;
+    ftString: ;
+    ftSmallint: ;
+    ftInteger: ;
+    ftWord: ;
+    ftBoolean: ;
+    ftFloat: pFDParam.AsFloat := pCurr;
+    ftCurrency: pFDParam.AsCurrency := pCurr;
+    ftBCD: pFDParam.AsBCD := pCurr;
+    ftDate: ;
+    ftTime: ;
+    ftDateTime: ;
+    ftBytes: ;
+    ftVarBytes: ;
+    ftAutoInc: ;
+    ftBlob: ;
+    ftMemo: ;
+    ftGraphic: ;
+    ftFmtMemo: ;
+    ftParadoxOle: ;
+    ftDBaseOle: ;
+    ftTypedBinary: ;
+    ftCursor: ;
+    ftFixedChar: ;
+    ftWideString: ;
+    ftLargeint: ;
+    ftADT: ;
+    ftArray: ;
+    ftReference: ;
+    ftDataSet: ;
+    ftOraBlob: ;
+    ftOraClob: ;
+    ftVariant: ;
+    ftInterface: ;
+    ftIDispatch: ;
+    ftGuid: ;
+    ftTimeStamp: ;
+    ftFMTBcd: pFDParam.AsFMTBCD := pCurr;
+    ftFixedWideChar: ;
+    ftWideMemo: ;
+    ftOraTimeStamp: ;
+    ftOraInterval: ;
+    ftLongWord: ;
+    ftShortint: ;
+    ftByte: ;
+    ftExtended: pFDParam.AsExtended := pCurr;
+    ftConnection: ;
+    ftParams: ;
+    ftStream: ;
+    ftTimeStampOffset: ;
+    ftObject: ;
+    ftSingle: pFDParam.AsSingle := pCurr;
   end;
 end;
 
