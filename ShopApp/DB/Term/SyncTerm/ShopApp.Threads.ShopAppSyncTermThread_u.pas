c93ca664d7908d527d9e62bd5333a241d496d191
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-10-31 17:49:50 -0300

    shopapp cria threadcreator

---
+++ ShopApp/DB/Term/SyncTerm/ShopApp.Threads.ShopAppSyncTermThread_u.pas
@@ -0,0 +1,19 @@
+unit ShopApp.Threads.ShopAppSyncTermThread_u;
+
+interface
+
+uses App.Threads.SyncTermThread_u, Sis.UI.IO.Output,
+  Sis.UI.IO.Output.ProcessLog, System.Classes, Sis.Threads.ThreadBas_u,
+  App.AppObj, Sis.Entities.Terminal, Sis.Threads.SafeBool;
+
+type
+  TShopAppAppSyncTermThread = class(TAppSyncTermThread)
+  private
+  protected
+
+  public
+  end;
+
+implementation
+
+end.
