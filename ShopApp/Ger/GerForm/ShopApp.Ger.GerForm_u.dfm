74fef2aacbd8ef2b64f15a057a173f98f49c1a9b
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-11-01 19:45:49 -0300

    shopapp gerform muda de pasta

---
+++ ShopApp/Ger/GerForm/ShopApp.Ger.GerForm_u.dfm
@@ -0,0 +1,18 @@
+inherited GerShopAppForm: TGerShopAppForm
+  Caption = 'GerShopAppForm'
+  TextHeight = 15
+  inherited BasePanel: TPanel
+    ExplicitTop = 389
+    inherited SempreVisivelCheckBox: TCheckBox
+      Left = 3
+      ExplicitLeft = 3
+    end
+    inherited AutoOpenCheckBox: TCheckBox
+      Left = 110
+      ExplicitLeft = 110
+    end
+  end
+  inherited StatusFrameScrollBox: TScrollBox
+    ExplicitHeight = 333
+  end
+end
---
+++ ShopApp/Ger/GerForm/ShopApp.Ger.GerForm_u.pas
@@ -0,0 +1,83 @@
+unit ShopApp.Ger.GerForm_u;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  App.Ger.GerForm_u, Vcl.ExtCtrls, System.Actions, Vcl.ActnList, Vcl.StdCtrls,
+  Vcl.ToolWin, Vcl.ComCtrls, App.AppObj, Sis.UI.Frame.Status.Thread_u,
+  Sis.Threads.Factory_u, ShopApp.Threads.ShopAppSyncTermThreadCreator_u, Sis.Entities.Terminal;
+
+type
+  TGerShopAppForm = class(TGerAppForm)
+  private
+    { Private declarations }
+    function FrameCreate(pTerminal: ITerminal): TThreadStatusFrame;
+    function ShopAppSyncTermThreadCreatorCreate(pFrame: TThreadStatusFrame; pTerminal: ITerminal):TShopAppSyncTermThreadCreator;
+  protected
+  public
+    { Public declarations }
+    constructor Create(AOwner: TComponent; pAppObj: IAppObj); reintroduce;
+  end;
+
+var
+  GerShopAppForm: TGerShopAppForm;
+
+implementation
+
+{$R *.dfm}
+
+uses Sis.Threads.ThreadCreator, Sis.Threads.SafeBool;
+
+{ TGerShopAppForm }
+
+constructor TGerShopAppForm.Create(AOwner: TComponent; pAppObj: IAppObj);
+var
+  sNomeLocal: string;
+begin
+  inherited;
+  sNomeLocal := AppObj.SisConfig.LocalMachineId.GetIdent;
+  AppObj.TerminalList.ExecuteForAll(
+    procedure(pTerminal: ITerminal)
+    var
+      oFrame: TThreadStatusFrame;
+      oCreator: TShopAppSyncTermThreadCreator;
+    begin
+      oFrame := FrameCreate(pTerminal);
+      oCreator := ShopAppSyncTermThreadCreatorCreate(oFrame, pTerminal);
+      oFrame.ThreadCreator := oCreator;
+    end, sNomeLocal);
+end;
+
+function TGerShopAppForm.FrameCreate(pTerminal: ITerminal): TThreadStatusFrame;
+var
+  oCreator: IThreadCreator;
+  sName: string;
+begin
+  // cria o Frame
+  sName := 'ThreadStatusFrame' + (StatusFrameScrollBox.ControlCount + 1).ToString;
+  Result := TThreadStatusFrame.Create(StatusFrameScrollBox);
+  Result.Name := sName;
+  Result.Parent := StatusFrameScrollBox;
+
+  FramesList.Add(Result);
+end;
+
+function TGerShopAppForm.ShopAppSyncTermThreadCreatorCreate(
+  pFrame: TThreadStatusFrame; pTerminal: ITerminal): TShopAppSyncTermThreadCreator;
+begin
+  // cria o creator
+  Result := TShopAppSyncTermThreadCreator.Create( //
+    pTerminal //
+    , AppObj //
+    , pFrame.Executando //
+    , pFrame.DoTerminate //
+    , pFrame.titOutput //
+    , pFrame.StatusOutput //
+    , pFrame.ProcessLog //
+    , pTerminal.AsText
+    );
+end;
+
+end.
