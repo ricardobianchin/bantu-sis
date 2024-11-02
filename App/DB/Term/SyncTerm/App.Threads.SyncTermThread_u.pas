590e6e8c5ef5193f796a485d6424a094e775877c
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-11-02 09:35:15 -0300

    Update App.Threads.SyncTermThread_u.pas
    - teste conecta e desconecta funcionou

--- App/DB/Term/SyncTerm/App.Threads.SyncTermThread_u.pas
+++ App/DB/Term/SyncTerm/App.Threads.SyncTermThread_u.pas
@@ -4,13 +4,18 @@ interface
 
 uses App.Threads.TermThread_u, Sis.UI.IO.Output, Sis.UI.IO.Output.ProcessLog,
   System.Classes, Sis.Threads.ThreadBas_u, App.AppObj, Sis.Entities.Terminal,
-  Sis.Threads.SafeBool;
+  Sis.Threads.SafeBool, Sis.DB.Factory, Sis.DB.DBTypes, App.DB.Utils,
+  Sis.Sis.Constants;
 
 type
   TAppSyncTermThread = class(TTermThread)
   private
+    FServCon, FTermCon: IDBConnection;
+    function Conectou: boolean;
+    procedure FecharConexoes;
   protected
     procedure Execute; override;
+    procedure SyncLoja; virtual;
 
   public
     constructor Create(pTerminal: ITerminal; pAppObj: IAppObj;
@@ -20,9 +25,42 @@ type
 
 implementation
 
-uses System.SysUtils;
+uses System.SysUtils, Sis.Config.SisConfig;
 
 { TAppSyncTermThread }
+
+function TAppSyncTermThread.Conectou: boolean;
+var
+  s: ISisConfig;
+  rDBConnectionParams: TDBConnectionParams;
+
+begin
+  s := AppObj.SisConfig;
+
+  rDBConnectionParams := TerminalIdToDBConnectionParams
+    (TERMINAL_ID_RETAGUARDA, AppObj);
+
+  FServCon := DBConnectionCreate('TAppSyncTermThread.serv.con', s,
+    rDBConnectionParams, ProcessLog, StatusOutput);
+
+  rDBConnectionParams.Server := Terminal.NomeNaRede;
+  rDBConnectionParams.Arq := Terminal.LocalArqDados;
+  rDBConnectionParams.Database := Terminal.Database;
+
+  FTermCon:= DBConnectionCreate('TAppSyncTermThread.term.con', s,
+    rDBConnectionParams, ProcessLog, StatusOutput);
+
+  Result := FServCon.Abrir;
+  if not Result then
+    exit;
+
+  Result := FTermCon.Abrir;
+  if not Result then
+  begin
+    FServCon.Fechar;
+    exit;
+  end;
+end;
 
 constructor TAppSyncTermThread.Create(pTerminal: ITerminal; pAppObj: IAppObj;
   pExecutandoSafeBool: ISafeBool; pTitOutput: IOutput; pStatusOutput: IOutput;
@@ -40,14 +78,40 @@ var
   i, m: integer;
 begin
   inherited;
-  m := 5+random(10);
+  if Terminated then
+    exit;
+  if not Conectou then
+    exit;
+  try
+    if Terminated then
+      exit;
+    StatusOutput.Exibir('Iniciou')
+  finally
+    FecharConexoes;
+  end;
+  {
+  m := 5 + random(10);
   for i := 1 to m do
   begin
-    OutputSafe(StatusOutput, i.ToString+'/'+m.ToString);
+    OutputSafe(StatusOutput, i.ToString + '/' + m.ToString);
     if Terminated then
       break;
     Sleep(1000);
   end;
+  }
+end;
+
+procedure TAppSyncTermThread.FecharConexoes;
+begin
+  FServCon.Fechar;
+  FServCon.Fechar;
+end;
+
+procedure TAppSyncTermThread.SyncLoja;
+begin
+Terminal.Database
+  SyncLoja(FServCon, FTermCon, appobj.CriticalSections.ServerDB,, pTermCrit: TFixecCriticalSection);
+
 end;
 
 end.
