eb2340486822336c8979b2b6c612f096e0e32379
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-11-03 16:37:23 -0300

    Update Sis.DB.DBExec_u.pas
    alinha codigo

--- Sis/DB/Sis.DB.DBExec_u.pas
+++ Sis/DB/Sis.DB.DBExec_u.pas
@@ -3,10 +3,8 @@ unit Sis.DB.DBExec_u;
 interface
 
 uses
-  FireDAC.Stan.Param
-  , System.Classes
-  , Data.DB, Sis.DB.DBSQLOperation_u, Sis.DB.DBTypes
-  ;
+  FireDAC.Stan.Param, System.Classes, Data.DB, Sis.DB.DBSQLOperation_u,
+  Sis.DB.DBTypes;
 
 type
   TDBExec = class(TDBSqlOperation, IDBExec)
