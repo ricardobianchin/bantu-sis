243475a5d0790bc23edaaa84cec273caa380964f
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-11-06 17:04:53 -0300

    Update Sis.DB.DBExecScript_u.pas
    ajusta metodos virtuais

--- Sis/DB/Sis.DB.DBExecScript_u.pas
+++ Sis/DB/Sis.DB.DBExecScript_u.pas
@@ -4,34 +4,107 @@ interface
 
 uses
   FireDAC.Stan.Param, System.Classes, Data.DB, Sis.DB.DBSQLOperation_u,
-  Sis.DB.DBTypes;
+  Sis.DB.DBTypes, System.SysUtils, Sis.Types, Sis.UI.IO.Output.ProcessLog,
+  Sis.UI.IO.Output, Sis.UI.IO.Output.ProcessLog.Registrador,
+  Sis.Threads.Crit.FixedCriticalSection_u;
 
 type
   TDBExecScript = class(TDBSqlOperation, IDBExecScript)
   private
+    FThreadSafe: Boolean;
+    FCritcalSection: TFixedCriticalSection;
+    FProcExecute: TProcedureOfObject;
+
+    function GetThreadSafe: Boolean;
+    procedure SetThreadSafe(const Value: Boolean);
+
+    function GetExecute: TProcedureOfObject;
+    procedure SetExecute(const Value: TProcedureOfObject);
+
+    property ProcExecute: TProcedureOfObject read GetExecute write SetExecute;
+
+    procedure ExecuteSafe;
   protected
+    procedure ExecuteNormal; virtual; abstract;
 
     function GetScriptTamanhoMaximo: integer; virtual; abstract;
-    function GetScriptTamanho:       integer; virtual; abstract;
+    function GetScriptTamanho: integer; virtual; abstract;
 
     property ScriptTamanhoMaximo: integer read GetScriptTamanhoMaximo;
-    property ScriptTamanho:       integer read GetScriptTamanho;
+    property ScriptTamanho: integer read GetScriptTamanho;
   public
-    procedure Execute; virtual; abstract;
+    procedure Execute;
     procedure PegueComando(pComando: string); virtual;
+    property ThreadSafe: Boolean read GetThreadSafe write SetThreadSafe;
+    constructor Create(pNomeComponente: string; pDBConnection: IDBConnection;
+      pProcessLog: IProcessLog; pOutput: IOutput;
+      pCritcalSection: TFixedCriticalSection; pThreadSafe: Boolean = True);
   end;
 
 implementation
 
 { TDBExecScript }
 
+constructor TDBExecScript.Create(pNomeComponente: string;
+  pDBConnection: IDBConnection; pProcessLog: IProcessLog; pOutput: IOutput;
+  pCritcalSection: TFixedCriticalSection; pThreadSafe: Boolean);
+begin
+  inherited Create(pNomeComponente, pDBConnection, pProcessLog, pOutput);
+
+  FCritcalSection := pCritcalSection;
+  if FCritcalSection = nil then
+    pThreadSafe := False;
+
+  SetThreadSafe(pThreadSafe);
+end;
+
+procedure TDBExecScript.Execute;
+begin
+  ProcExecute;
+end;
+
+procedure TDBExecScript.ExecuteSafe;
+begin
+  FCritcalSection.Acquire;
+  try
+    ExecuteNormal;
+  finally
+    FCritcalSection.Release;
+  end;
+end;
+
+function TDBExecScript.GetExecute: TProcedureOfObject;
+begin
+  Result := FProcExecute;
+end;
+
+function TDBExecScript.GetThreadSafe: Boolean;
+begin
+  Result := FThreadSafe;
+end;
+
 procedure TDBExecScript.PegueComando(pComando: string);
 var
   iTamanhoFuturo: integer;
 begin
-  iTamanhoFuturo := ScriptTamanho + 4{enteres} + Length(pComando);
-  if iTamanhoFuturo > iTamanhoFuturo then
+  iTamanhoFuturo := ScriptTamanho + 4 { enteres } + Length(pComando);
+  if iTamanhoFuturo > ScriptTamanhoMaximo then
     Execute;
+  //na classe filha deve ser fieto o add pcommand
+end;
+
+procedure TDBExecScript.SetExecute(const Value: TProcedureOfObject);
+begin
+  FProcExecute := Value;
+end;
+
+procedure TDBExecScript.SetThreadSafe(const Value: Boolean);
+begin
+  FThreadSafe := Value;
+  if Value then
+    FProcExecute := ExecuteSafe
+  else
+    FProcExecute := ExecuteNormal;
 end;
 
 end.
