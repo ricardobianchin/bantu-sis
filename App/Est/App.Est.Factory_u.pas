0ea01aaaf7be8b098cefedbb61a2cfe18e862392
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-11-23 15:02:29 -0300

    Update App.Est.Factory_u.pas
    retira creates de SessaoCaixa

--- App/Est/App.Est.Factory_u.pas
+++ App/Est/App.Est.Factory_u.pas
@@ -3,12 +3,9 @@ unit App.Est.Factory_u;
 interface
 
 uses App.Est.Prod.Barras.DBI, Sis.DB.DBTypes, System.Classes,
-  Sis.Entities.Types, App.Est.Venda.CaixaSessao.DBI, Sis.Usuario;
+  Sis.Entities.Types;
 
 function AppEstBarrasDBICreate(pDBConnection: IDBConnection): IBarrasDBI;
-function CaixaSessaoDBICreate(pDBConnection: IDBConnection;
-  pLogUsuario: IUsuario; pLojaId: TLojaId; pTerminalId: TTerminalId;
-  pMachineIdentId: smallint): ICaixaSessaoDBI;
 
 implementation
 
@@ -17,14 +14,6 @@ uses App.Est.Prod.Barras.DBI_u, App.Est.Venda.CaixaSessao.DBI_u;
 function AppEstBarrasDBICreate(pDBConnection: IDBConnection): IBarrasDBI;
 begin
   Result := TBarrasDBI.Create(pDBConnection);
-end;
-
-function CaixaSessaoDBICreate(pDBConnection: IDBConnection;
-  pLogUsuario: IUsuario; pLojaId: TLojaId; pTerminalId: TTerminalId;
-  pMachineIdentId: smallint): ICaixaSessaoDBI;
-begin
-  Result := TCaixaSessaoDBI.Create(pDBConnection, pLogUsuario, pLojaId,
-    pTerminalId, pMachineIdentId);
 end;
 
 end.
