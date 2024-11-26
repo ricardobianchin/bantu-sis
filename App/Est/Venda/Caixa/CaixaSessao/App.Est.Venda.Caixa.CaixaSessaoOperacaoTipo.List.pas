ceebeec008aff6245792298023ae087782bbf489
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-11-26 15:59:28 -0300

    ajusta operacao tipo

--- App/Est/Venda/Caixa/CaixaSessao/App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.List.pas
+++ App/Est/Venda/Caixa/CaixaSessao/App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.List.pas
@@ -2,15 +2,17 @@ unit App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.List;
 
 interface
 
-uses Sis.Lists.IdCharHashList, App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo;
+uses Sis.Lists.IdCharHashList, App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo,
+  App.Est.Venda.Caixa.CaixaSessao.Utils_u, System.Classes;
 
 type
-  ICxOperacaoTipoList = interface(IIdCharHashList)
+  ICxOperacaoTipoList = interface(IInterfaceList)
     ['{947DB0B6-A760-4625-8DE4-0849E8A88CD3}']
-    function GetCxOperacaoTipo(Index: integer)
-      : ICxOperacaoTipo;
+    function GetCxOperacaoTipo(Index: integer): ICxOperacaoTipo;
     property CxOperacaoTipo[Index: integer]: ICxOperacaoTipo
       read GetCxOperacaoTipo; default;
+
+    function CxOpTipoToOperacaoTipo(pCxOpTipo: TCxOpTipo): ICxOperacaoTipo;
   end;
 
 implementation
--- App/Est/Venda/Caixa/CaixaSessao/App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.List_u.pas
+++ App/Est/Venda/Caixa/CaixaSessao/App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.List_u.pas
@@ -3,24 +3,43 @@ unit App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.List_u;
 interface
 
 uses App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.List,
-  App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo, System.Classes;
+  App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo, System.Classes,
+  App.Est.Venda.Caixa.CaixaSessao.Utils_u;
 
 type
   TCxOperacaoTipoList = class(TInterfaceList, ICxOperacaoTipoList)
   private
-    function GetCxOperacaoTipo(Index: integer)
-      : ICxOperacaoTipo;
+    function GetCxOperacaoTipo(Index: integer): ICxOperacaoTipo;
   public
     property CxOperacaoTipo[Index: integer]: ICxOperacaoTipo
       read GetCxOperacaoTipo; default;
+    function CxOpTipoToOperacaoTipo(pCxOpTipo: TCxOpTipo): ICxOperacaoTipo;
   end;
 
 implementation
 
 { TCxOperacaoTipoList }
 
-function TCxOperacaoTipoList.GetCxOperacaoTipo(
-  Index: integer): ICxOperacaoTipo;
+function TCxOperacaoTipoList.CxOpTipoToOperacaoTipo(pCxOpTipo: TCxOpTipo)
+  : ICxOperacaoTipo;
+var
+  ot: ICxOperacaoTipo;
+  i: integer;
+begin
+  Result := nil;
+
+  for i := 0 to Count - 1 do
+  begin
+    ot := CxOperacaoTipo[i];
+    if ot.Id = pCxOpTipo then
+    begin
+      Result := ot;
+      break;
+    end;
+  end;
+end;
+
+function TCxOperacaoTipoList.GetCxOperacaoTipo(Index: integer): ICxOperacaoTipo;
 begin
   Result := ICxOperacaoTipo(Items[Index]);
 end;
