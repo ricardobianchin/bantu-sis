bede9757c9c7322e04bdd12f01acee3fb5befbc3
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-11-27 22:30:21 -0300

    Update App.Est.Venda.Caixa.CaixaSessaoOperacao.Action_u.pas
    cria CxOperacaoEnt

--- App/Est/Venda/Caixa/CaixaSessao/CxOperacao/App.Est.Venda.Caixa.CaixaSessaoOperacao.Action_u.pas
+++ App/Est/Venda/Caixa/CaixaSessao/CxOperacao/App.Est.Venda.Caixa.CaixaSessaoOperacao.Action_u.pas
@@ -3,24 +3,28 @@ unit App.Est.Venda.Caixa.CaixaSessaoOperacao.Action_u;
 interface
 
 uses Vcl.ActnList, App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo.DBI,
-  App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo, System.Classes;
+  App.Est.Venda.Caixa.CaixaSessaoOperacaoTipo, System.Classes, App.Est.Venda.Caixa.CaixaSessaoOperacao.Ent;
 
 type
   TCxOperacaoAction = class(TAction)
   private
     FCxOperacaoTipo: ICxOperacaoTipo;
     FCxOperacaoTipoDBI: ICxOperacaoTipoDBI;
+    FCxOperacaoEnt: ICxOperacaoEnt;
   protected
   public
     procedure AjusteEnabled;
     procedure ExecuteTarget(Target: TObject); override;
     function HandlesTarget(Target: TObject): Boolean; override;
     property CxOperacaoTipo: ICxOperacaoTipo read FCxOperacaoTipo;
+    property CxOperacaoEnt: ICxOperacaoEnt read FCxOperacaoEnt;
     constructor Create(AOwner: TComponent; pCxOperacaoTipo: ICxOperacaoTipo;
-      pCxOperacaoTipoDBI: ICxOperacaoTipoDBI); reintroduce;
+      pCxOperacaoTipoDBI: ICxOperacaoTipoDBI; pCxOperacaoEnt: ICxOperacaoEnt); reintroduce;
   end;
 
 implementation
+
+uses Vcl.Dialogs;
 
 { TCxOperacaoAction }
 
@@ -30,12 +34,12 @@ begin
 end;
 
 constructor TCxOperacaoAction.Create(AOwner: TComponent;
-  pCxOperacaoTipo: ICxOperacaoTipo; pCxOperacaoTipoDBI: ICxOperacaoTipoDBI);
+  pCxOperacaoTipo: ICxOperacaoTipo; pCxOperacaoTipoDBI: ICxOperacaoTipoDBI; pCxOperacaoEnt: ICxOperacaoEnt);
 begin
   inherited Create(AOwner);
   FCxOperacaoTipo := pCxOperacaoTipo;
   FCxOperacaoTipoDBI := pCxOperacaoTipoDBI;
-
+  FCxOperacaoEnt := pCxOperacaoEnt;
   Name := 'CxOperacao' + FCxOperacaoTipo.Name + 'Ins';
   Caption := FCxOperacaoTipo.Caption;
   Hint := FCxOperacaoTipo.Hint;
@@ -44,7 +48,7 @@ end;
 procedure TCxOperacaoAction.ExecuteTarget(Target: TObject);
 begin
   inherited;
-
+  Showmessage('a');
 end;
 
 function TCxOperacaoAction.HandlesTarget(Target: TObject): Boolean;
