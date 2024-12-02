3c5ff031fcec3cde0245a5443661a56284397bcf
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-12-02 01:17:44 -0300

    sis cria Atualizavel e WinControlNavigator

---
+++ Sis/Sis/Sis.Sis.Atualizavel_u.pas
@@ -0,0 +1,13 @@
+unit Sis.Sis.Atualizavel_u;
+
+interface
+
+type
+  IAtualizavel = interface(IInterface)
+    ['{5CA545CB-6C9D-4C49-A4D0-AB93FFCC16C7}']
+    procedure Atualize;
+  end;
+
+implementation
+
+end.
---
+++ Sis/UI/Controls/Sis.UI.Controls.WinControlsNavigator.pas
@@ -0,0 +1,16 @@
+unit Sis.UI.Controls.WinControlsNavigator;
+
+interface
+
+type
+  IWinControlsNavigator = interface(IInterface)
+    ['{A910B182-94D6-4AD0-8FDE-94FA9608868E}']
+    procedure SelecionePrimeiro;
+    procedure SelecioneAnterior;
+    procedure SelecioneProximo;
+    procedure SelecioneUltimo;
+  end;
+
+implementation
+
+end.
