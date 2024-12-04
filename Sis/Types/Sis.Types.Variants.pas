c8a380b0a58cabff17ba34c396d7f9ad5a36ea9f
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-12-04 18:08:40 -0300

    Update Sis.Types.Variants.pas
    cria ValuesToVarArray

--- Sis/Types/Sis.Types.Variants.pas
+++ Sis/Types/Sis.Types.Variants.pas
@@ -3,6 +3,8 @@ unit Sis.Types.Variants;
 interface
 
 function VarToVarArray(const Value: Variant): Variant;
+function ValuesToVarArray(const Values: array of Variant): Variant;
+function VarArrayVazia(const Value: Variant): Boolean;
 
 implementation
 
@@ -12,6 +14,32 @@ function VarToVarArray(const Value: Variant): Variant;
 begin
   Result := VarArrayCreate([0, 0], varVariant);
   Result[0] := Value;
+end;
+
+function ValuesToVarArray(const Values: array of Variant): Variant;
+var
+  i: Integer;
+begin
+  Result := VarArrayCreate([0, High(Values)], varVariant);
+  for i := Low(Values) to High(Values) do
+  begin
+    Result[i] := Values[i];
+  end;
+end;
+
+function VarArrayVazia(const Value: Variant): Boolean;
+var
+  iMaisBaixo: integer;
+  iMaisAlto: integer;
+begin
+  Result := not VarIsArray(Value);
+  if Result then
+    exit;
+
+  iMaisBaixo := VarArrayLowBound(Value, 1);
+  iMaisAlto := VarArrayHighBound(Value, 1);
+
+  Result := iMaisBaixo > iMaisAlto;
 end;
 
 end.
