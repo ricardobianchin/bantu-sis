6830ca1560998a161e170c2fe25cb25de08ee6dd
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-12-17 21:52:38 -0300

    shopapp vendaframe
    - caretlabel -> caretshape
    - DimensioneControles

--- ShopApp/UI/VendaFrame/ShopApp.UI.PDV.VendaFrame_u.dfm
+++ ShopApp/UI/VendaFrame/ShopApp.UI.PDV.VendaFrame_u.dfm
@@ -3,4 +3,50 @@ inherited ShopVendaPDVFrame: TShopVendaPDVFrame
   Height = 311
   ExplicitWidth = 542
   ExplicitHeight = 311
+  inherited MeioPanel: TPanel
+    Width = 542
+    Height = 311
+    ExplicitWidth = 542
+    ExplicitHeight = 311
+    object InputPanel: TPanel
+      Left = 0
+      Top = 189
+      Width = 513
+      Height = 65
+      BevelOuter = bvLowered
+      Caption = '  '
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -33
+      Font.Name = 'Segoe UI'
+      Font.Style = [fsBold]
+      ParentFont = False
+      TabOrder = 0
+      DesignSize = (
+        513
+        65)
+      object BuscaLabel: TLabel
+        Left = 42
+        Top = 2
+        Width = 467
+        Height = 45
+        Alignment = taRightJustify
+        Anchors = [akTop, akRight]
+        Caption = 'Quantidade * C'#243'digo / C'#243'digo'
+      end
+      object CaretShape: TShape
+        Left = 490
+        Top = 47
+        Width = 19
+        Height = 5
+        Brush.Color = clBtnText
+        Pen.Style = psClear
+      end
+    end
+  end
+  object CaretTimer: TTimer
+    OnTimer = CaretTimerTimer
+    Left = 152
+    Top = 32
+  end
 end
--- ShopApp/UI/VendaFrame/ShopApp.UI.PDV.VendaFrame_u.pas
+++ ShopApp/UI/VendaFrame/ShopApp.UI.PDV.VendaFrame_u.pas
@@ -3,15 +3,24 @@ unit ShopApp.UI.PDV.VendaFrame_u;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, App.UI.PDV.VendaBasFrame_u;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  App.UI.PDV.VendaBasFrame_u, Vcl.ExtCtrls, Vcl.StdCtrls, System.Types;
 
 type
   TShopVendaPDVFrame = class(TVendaBasPDVFrame)
+    InputPanel: TPanel;
+    BuscaLabel: TLabel;
+    CaretTimer: TTimer;
+    CaretShape: TShape;
+    procedure CaretTimerTimer(Sender: TObject);
   private
     { Private declarations }
+    FColuna1Rect, FColuna2Rect: TRect;
+    procedure DimensioneInput;
   public
     { Public declarations }
+    procedure DimensioneControles; override;
   end;
 
 var
@@ -20,5 +29,55 @@ var
 implementation
 
 {$R *.dfm}
+{ TShopVendaPDVFrame }
+
+procedure TShopVendaPDVFrame.CaretTimerTimer(Sender: TObject);
+begin
+  inherited;
+  CaretShape.Visible := not CaretShape.Visible;
+end;
+
+procedure TShopVendaPDVFrame.DimensioneControles;
+var
+  MargHor: integer;
+  MargVer: integer;
+begin
+  inherited;
+  MargHor := 5;
+  MargVer := 3;
+
+  FColuna1Rect.Left := MargHor;
+  FColuna1Rect.Top := MargVer;
+  FColuna1Rect.Width := (MeioPanel.Width - (3 * MargHor)) div 2;
+  FColuna1Rect.Height := MeioPanel.Height - (2 * MargVer);
+
+  FColuna2Rect.Left := FColuna1Rect.Width + MargHor * 2;
+  FColuna2Rect.Top := FColuna1Rect.Top;
+  FColuna2Rect.Width := FColuna1Rect.Width;
+  FColuna2Rect.Height := FColuna1Rect.Height;
+
+  DimensioneInput;
+end;
+
+procedure TShopVendaPDVFrame.DimensioneInput;
+var
+  l, t, w, h: integer;
+begin
+  l := FColuna2Rect.Left;
+  h := FColuna2Rect.Height div 8;
+  t := FColuna2Rect.Height - h;
+  w := FColuna2Rect.Width;
+
+  InputPanel.Left := l;
+  InputPanel.Top := t;
+  InputPanel.Width := w;
+  InputPanel.Height := h;
+
+  InputPanel.Color := Rgb(16, 21, 36);
+  InputPanel.BevelOuter := bvNone;
+  InputPanel.Font.Color := Rgb(248, 237, 228);
+
+  CaretShape.Brush.Color := InputPanel.Font.Color;
+end;
 
 end.
