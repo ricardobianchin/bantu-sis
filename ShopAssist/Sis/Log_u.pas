ea426d9b555667ca411ec3918de49e3214597774
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-12-28 13:34:42 -0300

    ShopAssist
    muda log

--- ShopAssist/Sis/Log_u.pas
+++ ShopAssist/Sis/Log_u.pas
@@ -11,25 +11,28 @@ uses Sis_u, System.SysUtils, Sis.UI.IO.Files, Sis.Types.Utils_u,
   System.DateUtils;
 
 var
-  dDtHoraInicio: TDateTime;
-  sNomeArq: string;
   sPastaLog: string;
 
 procedure InicieLog;
 var
+  dAgora: TDateTime;
   sAgora: string;
+  dHoje: TDateTime;
   sMens: string;
+  sNomeArq: string;
 begin
-  dDtHoraInicio := Now;
-  sAgora := FormatDateTime('dd/mm/yyyy hh:nn:ss,zzz', dDtHoraInicio);
-
   sPastaLog := sPastaTmp + 'Assist\';
   GarantirPasta(sPastaLog);
-  sNomeArq := sPastaLog + 'Log Assist ' + DateTimeToNomeArq
-    (dDtHoraInicio) + '.txt';
 
+  dAgora := Now;
+  dHoje := Trunc(dAgora);
+
+  sNomeArq := sPastaLog + 'Log Assist ' + DateToNomeArq(dHoje) + '.txt';
+
+  sAgora := FormatDateTime('dd/mm/yyyy hh:nn:ss,zzz', dAgora);
   sMens := '--------------------------------------------' + sNOVA_LINHA + sAgora
     + ';' + 'Inicio' + sNOVA_LINHA;
+
   AdicioneAoArquivo(sMens, sNomeArq);
 end;
 
@@ -37,11 +40,16 @@ procedure EscrevaLog(pFrase: string);
 var
   dAgora: TDateTime;
   sAgora: string;
+  dHoje: TDateTime;
+  sNomeArq: string;
 begin
   dAgora := Now;
+  dHoje := Trunc(dAgora);
+
+  sNomeArq := sPastaLog + 'Log Assist ' + DateToNomeArq(dHoje) + '.txt';
+
   sAgora := FormatDateTime('dd/mm/yyyy hh:nn:ss,zzz', dAgora);
   AdicioneAoArquivo(sAgora + ';' + pFrase + sNOVA_LINHA, sNomeArq);
-
 end;
 
 end.
