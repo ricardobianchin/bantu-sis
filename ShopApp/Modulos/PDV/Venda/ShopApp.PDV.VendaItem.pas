2dfe8d3e691110577f20f72b3217aaa2f79a0c0c
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-01-06 05:14:36 -0300

    shop pdv vendaitem, criaAsStringFita

--- ShopApp/Modulos/PDV/Venda/ShopApp.PDV.VendaItem.pas
+++ ShopApp/Modulos/PDV/Venda/ShopApp.PDV.VendaItem.pas
@@ -7,6 +7,13 @@ uses App.PDV.VendaItem;
 type
   IShopPDVVendaItem = interface(IPDVVendaItem)
   ['{7A9499FF-8027-4989-8FD0-10842C8B8392}']
+    function GetAsLinha1: string;
+    function GetAsLinha2: string;
+    function GetAsLinha3: string;
+
+    property AsLinha1: string read GetAsLinha1;
+    property AsLinha2: string read GetAsLinha2;
+    property AsLinha3: string read GetAsLinha3;
   end;
 
 implementation
--- ShopApp/Modulos/PDV/Venda/ShopApp.PDV.VendaItem_u.pas
+++ ShopApp/Modulos/PDV/Venda/ShopApp.PDV.VendaItem_u.pas
@@ -7,9 +7,65 @@ uses App.PDV.VendaItem_u, ShopApp.PDV.VendaItem;
 type
   TShopPDVVendaItem = class(TPDVVendaItem, IShopPDVVendaItem)
   private
+    function GetAsLinha1: string;
+    function GetAsLinha2: string;
+    function GetAsLinha3: string;
+
+    property AsLinha1: string read GetAsLinha1;
+    property AsLinha2: string read GetAsLinha2;
+    property AsLinha3: string read GetAsLinha3;
+  protected
+    function GetAsStringFita: string; override;
   public
   end;
 
 implementation
+
+uses Sis.Types.Bool_u, Sis.Types.Floats, System.Math, System.SysUtils;
+{ TShopPDVVendaItem }
+
+function TShopPDVVendaItem.GetAsLinha1: string;
+begin
+  Result := Prod.IdAsStrZero + '  ' + Prod.DescrRed;
+end;
+
+function TShopPDVVendaItem.GetAsLinha2: string;
+var
+  sQtd: string;
+  sPrecoUnit: string;
+  sPreco: string;
+  sDesconto: string;
+begin
+  sQtd := FormatFloat('###,##0.###', Qtd);
+  sPrecoUnit := DinhToStr(PrecoUnit);
+  sPreco := DinhToStr(Preco);
+  sDesconto := DinhToStr(Desconto);
+
+  Result := ' ' + sQtd //
+    + ' ' + Prod.UnidSigla //
+    + ' x ' + sPrecoUnit //
+    ;
+
+  if Desconto >= 0.01 then
+  begin
+    Result := Result + ' - ' + sDesconto;
+  end;
+
+  Result := Result + ' = ' + sPreco;
+end;
+
+function TShopPDVVendaItem.GetAsLinha3: string;
+begin
+  Result := Iif(Cancelado, '     C A N C E L A D O', '');
+end;
+
+function TShopPDVVendaItem.GetAsStringFita: string;
+begin
+  Result := //
+    GetAsLinha1 + #13#10 //
+    + GetAsLinha2 + #13#10 //
+    + GetAsLinha3 + #13#10 //
+    ;
+end;
 
 end.
