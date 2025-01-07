b7415b69b428816e819bf9ff40e7db992e974dfe
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-01-07 09:44:47 -0300

    app pdv dbi, cria `PagSomenteDinheiro`

--- App/Modulos/PDV/App.PDV.DBI.pas
+++ App/Modulos/PDV/App.PDV.DBI.pas
@@ -7,6 +7,7 @@ uses Sis.DBI;
 type
   IAppPDVDBI = interface(IDBI)
     ['{DF20E6A5-9D46-40EC-B9E2-4102BC97981F}']
+    procedure PagSomenteDinheiro;
   end;
 
 implementation
--- App/Modulos/PDV/App.PDV.DBI_u.pas
+++ App/Modulos/PDV/App.PDV.DBI_u.pas
@@ -14,13 +14,17 @@ type
   protected
     property AppObj: IAppObj read FAppObj;
     property Terminal: ITerminal read FTerminal;
-    property PDVVenda: IPDVVenda read FPDVVenda;
+    property PDVVenda: IPDVVenda read FPdvVenda;
   public
+    procedure PagSomenteDinheiro;
+
     constructor Create(pDBConnection: IDBConnection; pAppObj: IAppObj;
       pTerminal: ITerminal; pPdvVenda: IPDVVenda);
   end;
 
 implementation
+
+uses Data.DB, System.SysUtils, Sis.Entities.Types;
 
 { TAppPDVDBI }
 
@@ -30,7 +34,43 @@ begin
   inherited Create(pDBConnection);
   FAppObj := pAppObj;
   FTerminal := pTerminal;
-  FPDVVenda := pPdvVenda;
+  FPdvVenda := pPdvVenda;
+end;
+
+procedure TAppPDVDBI.PagSomenteDinheiro;
+var
+  sSql: string;
+  V: IPDVVenda;
+  q: TDataSet;
+begin
+  V := FPdvVenda;
+
+  sSql := //
+    'SELECT'#13#10 //
+
+    + 'FINALIZADO_EM_RET'#13#10 //
+    + ', DESCONTO_TOTAL_RET'#13#10 //
+    + ', CUSTO_TOTAL_RET'#13#10 //
+    + ', TOTAL_LIQUIDO_RET'#13#10 //
+
+    + 'FROM VENDA_PAG_INS_PA.FINALIZE_SO_DIN'#13#10 //
+
+    + '('#13#10 //
+    + '  ' + V.Loja.Id.ToString + ' -- LOJA_ID'#13#10 //
+    + '  , ' + V.TerminalId.ToString + ' -- TERMINAL_ID'#13#10 //
+    + '  , ' + V.EstMovId.ToString + ' -- EST_MOV_ID'#13#10 //
+    + ');';
+
+  // {$IFDEF DEBUG}
+  // CopyTextToClipboard(sSql);
+  // {$ENDIF}
+
+  DBConnection.Abrir;
+  try
+    DBConnection.QueryDataSet(sSql, q);
+  finally
+    DBConnection.Fechar;
+  end;
 end;
 
 end.
