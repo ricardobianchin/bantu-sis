c062def0db03953209e6933f0abddda01a441eb7
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-02-20 07:05:19 -0300

    Update Sis.Entities.Types.pas

--- Sis/Entities/Sis.Entities.Types.pas
+++ Sis/Entities/Sis.Entities.Types.pas
@@ -18,6 +18,9 @@ type
     function ToStrZero: string;
   end;
 
+function GetCod(pLojaId: TLojaId; pTerminalId: TTerminalId;
+  pId: integer; pPrefixo: string = ''; pSeparador: string = '-'): string;
+
 implementation
 
 uses System.SysUtils;
@@ -44,6 +47,16 @@ end;
 function TLojaIdHelper.ToStrZero: string;
 begin
   Result := Format('%.3d', [Self]);
+end;
+
+function GetCod(pLojaId: TLojaId; pTerminalId: TTerminalId;
+  pId: integer; pPrefixo: string; pSeparador: string): string;
+var
+  sFormat: string;
+begin
+  sFormat := '%d' + pSeparador + '.2%d' + pSeparador + '%.7d';
+  Result := pPrefixo + pSeparador + Format(sFormat,
+    [pLojaId, pTerminalId, pId]);
 end;
 
 end.
