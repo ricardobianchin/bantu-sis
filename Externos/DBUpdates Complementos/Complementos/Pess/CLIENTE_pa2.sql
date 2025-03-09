98023c99a4335c55a0fe60641fe2fe23880fbc19
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-03-08 23:56:23 -0300

    indo testar cliente

---
+++ Externos/DBUpdates Complementos/Complementos/Pess/CLIENTE_pa2.sql
@@ -0,0 +1,19 @@
+/*
+in "C:\Pr\app\bantu\bantu-sis\Src\Externos\DBUpdates Complementos\Complementos\Pess\pessoa_pa2.sql";
+*/
+
+SET TERM ^;
+CREATE OR ALTER PACKAGE CLIENTE_PA
+AS
+BEGIN
+END^
+
+---------------------------
+---    BODY
+---------------------------
+
+RECREATE PACKAGE BODY CLIENTE_PA
+AS
+BEGIN
+END^
+SET TERM ;^
