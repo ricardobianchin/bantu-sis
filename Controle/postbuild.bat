f5f52ea8319868c6bff68de82b33a955b5dfacde
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-03-12 13:21:51 -0300

    conserta build events

--- Controle/postbuild.bat
+++ Controle/postbuild.bat
@@ -1,10 +1,19 @@
-"C:\Program Files\FreeFileSync\FreeFileSync.exe" "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu db updates Exe BatchRun.ffs_batch"
-"C:\Program Files\FreeFileSync\FreeFileSync.exe" "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu db updates Inst BatchRun.ffs_batch"
+REM ------------ VARS --------------
+SET EXE_FILE = "D:\App\Arch\FreeFileSync\FreeFileSync.exe"
+REM SET EXE_FILE = "C:\Program Files\FreeFileSync\FreeFileSync.exe"
 
-"C:\Program Files\FreeFileSync\FreeFileSync.exe" "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu tabviews Exe.ffs_batch"
-"C:\Program Files\FreeFileSync\FreeFileSync.exe" "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu tabviews Inst.ffs_batch"
+REM --------------------------------
 
- pause
+REM --------- UPDATES -------------
+%EXE_FILE% "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu db updates Exe BatchRun.ffs_batch"
+%EXE_FILE% "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu db updates Inst BatchRun.ffs_batch"
+
+REM ---------- ENVIA EXE ----------
+%EXE_FILE% "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu tabviews Exe.ffs_batch"
+%EXE_FILE% "C:\Pr\app\bantu\bantu-sis\Src\Controle\atu tabviews Inst.ffs_batch"
+
+
+rem pause
 
 rem copy "C:\Pr\app\bantu\bantu-sis\Src\Externos\DBUpdates\000\000" "C:\Pr\app\bantu\bantu-sis\Exe\Inst\Update\DBUpdates\000\000"
 rem pause
