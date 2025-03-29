6e5eaa85d25b559ca9e684dec6749af829f41ec7
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-03-29 06:37:37 -0300

    app modu config, apaga metodos e controles

--- App/Modulos/Config/App.UI.Form.Bas.Modulo.Config_u.dfm
+++ App/Modulos/Config/App.UI.Form.Bas.Modulo.Config_u.dfm
@@ -13,96 +13,22 @@ inherited ConfigModuloBasForm: TConfigModuloBasForm
   end
   inherited BasePanel: TPanel
     Width = 604
+    StyleElements = [seFont, seClient, seBorder]
     ExplicitWidth = 604
+    DesignSize = (
+      604
+      29)
     inherited StatusPanel1: TPanel
+      StyleElements = [seFont, seClient, seBorder]
+      inherited StatusLabel1: TLabel
+        StyleElements = [seFont, seClient, seBorder]
+      end
       inherited OutputLabel: TLabel
         Width = 203
         Height = 27
+        StyleElements = [seFont, seClient, seBorder]
       end
     end
-  end
-  object TopoPanel: TPanel [2]
-    Left = 0
-    Top = 30
-    Width = 604
-    Height = 75
-    Align = alTop
-    BevelOuter = bvNone
-    Caption = ' '
-    TabOrder = 2
-    object MenuPageControl: TPageControl
-      Left = 0
-      Top = 0
-      Width = 604
-      Height = 66
-      ActivePage = ConfigAmbienteTabSheet
-      Align = alTop
-      TabOrder = 0
-      object ConfigAmbienteTabSheet: TTabSheet
-        Caption = 'Ambiente'
-        ImageIndex = 1
-        object ConfigAmbienteToolBar: TToolBar
-          Left = 0
-          Top = 0
-          Width = 596
-          Height = 21
-          AutoSize = True
-          ButtonHeight = 21
-          ButtonWidth = 104
-          Caption = 'EstoqueToolBar'
-          Ctl3D = False
-          EdgeInner = esNone
-          EdgeOuter = esNone
-          List = True
-          ShowCaptions = True
-          TabOrder = 0
-          object ConfigAmbienteLojasToolButton: TToolButton
-            Left = 0
-            Top = 0
-            Action = ConfigAmbiLojasAction
-          end
-          object ConfigTerminaisToolButton: TToolButton
-            Left = 104
-            Top = 0
-            Action = ConfigTerminaisAction
-          end
-        end
-      end
-      object ConfigImportTabSheet: TTabSheet
-        Caption = 'Importar Dados'
-        object ImportOrigemTitLabel: TLabel
-          Left = 2
-          Top = 11
-          Width = 40
-          Height = 15
-          Caption = 'Origem'
-        end
-        object DBImportOrigemComboBox: TComboBox
-          Left = 47
-          Top = 8
-          Width = 211
-          Height = 23
-          TabOrder = 0
-          Text = 'DBImportOrigemComboBox'
-        end
-        object ConfigDBImportButton: TButton
-          Left = 263
-          Top = 7
-          Width = 124
-          Height = 25
-          Action = ConfigDBImportAbrirAction
-          TabOrder = 1
-        end
-      end
-    end
-  end
-  object PageControl1: TPageControl [3]
-    Left = 0
-    Top = 105
-    Width = 604
-    Height = 342
-    Align = alClient
-    TabOrder = 3
   end
   inherited ShowTimer_BasForm: TTimer
     Left = 160
@@ -122,17 +48,14 @@ inherited ConfigModuloBasForm: TConfigModuloBasForm
     object ConfigDBImportAbrirAction: TAction
       Caption = 'Abrir Importa'#231#227'o...'
       Hint = 'Abre a importa'#231#227'o da oridem selecionada'
-      OnExecute = ConfigDBImportAbrirActionExecute
     end
     object ConfigAmbiLojasAction: TAction
       Caption = 'Estabelecimentos'
       Hint = 'Cadastro dos estabelecimentos atual e rede'
-      OnExecute = ConfigAmbiLojasActionExecute
     end
     object ConfigTerminaisAction: TAction
       Caption = 'Terminais'
       Hint = 'Castastro dos terminais da loja atual'
-      OnExecute = ConfigTerminaisActionExecute
     end
   end
   object BalloonHint1: TBalloonHint
--- App/Modulos/Config/App.UI.Form.Bas.Modulo.Config_u.pas
+++ App/Modulos/Config/App.UI.Form.Bas.Modulo.Config_u.pas
@@ -15,43 +15,14 @@ uses
 
 type
   TConfigModuloBasForm = class(TModuloBasForm)
-    TopoPanel: TPanel;
     ConfigActionList: TActionList;
-    MenuPageControl: TPageControl;
-    ConfigImportTabSheet: TTabSheet;
-    DBImportOrigemComboBox: TComboBox;
-    ImportOrigemTitLabel: TLabel;
-    ConfigDBImportButton: TButton;
     ConfigDBImportAbrirAction: TAction;
-    ConfigAmbienteTabSheet: TTabSheet;
-    ConfigAmbienteToolBar: TToolBar;
-    ConfigAmbienteLojasToolButton: TToolButton;
-    ConfigTerminaisToolButton: TToolButton;
     ConfigAmbiLojasAction: TAction;
     ConfigTerminaisAction: TAction;
-    PageControl1: TPageControl;
     BalloonHint1: TBalloonHint;
-
-    procedure ShowTimer_BasFormTimer(Sender: TObject);
-
-    // dbimport
-    procedure ConfigDBImportAbrirActionExecute(Sender: TObject);
-    procedure ConfigAmbiLojasActionExecute(Sender: TObject);
-    procedure ConfigTerminaisActionExecute(Sender: TObject);
   private
     { Private declarations }
-    FFormClassNamesSL: TStringList;
-    FContador: IContador;
-    FOutputNotify: IOutput;
-
-    FAmbiLojasDataSetFormCreator: IFormCreator;
-    FAmbiTerminalDataSetFormCreator: IFormCreator;
-
-    procedure TabSheetCrie(pFormCreator: IFormCreator);
   protected
-    procedure DBImportPrep; virtual;
-    function DBImportFormCreate(pItemIndex: integer): TDBImportForm;
-      virtual; abstract;
   public
     { Public declarations }
     constructor Create(AOwner: TComponent; pModuloSistema: IModuloSistema;
@@ -75,133 +46,17 @@ constructor TConfigModuloBasForm.Create(AOwner: TComponent;
   pModuloSistema: IModuloSistema; pEventosDeSessao: IEventosDeSessao;
   pSessaoIndex: TSessaoIndex; pUsuario: IUsuario; pAppObj: IAppObj;
   pTerminalId: TTerminalId);
-var
-  oAppInfo: IAppInfo;
-  oSisConfig: ISisConfig;
 begin
   inherited Create(AOwner, pModuloSistema, pEventosDeSessao, pSessaoIndex,
     pUsuario, pAppObj, pTerminalId);
-  DBImportPrep;
-  FFormClassNamesSL := TStringList.Create;
-
-  FContador := ContadorCreate;
-  FOutputNotify := BalloonHintOutputCreate(BalloonHint1);
-
-  FAmbiLojasDataSetFormCreator := AmbiLojaDataSetFormCreatorCreate
-    (FFormClassNamesSL, LogUsuario, DBMS, Output, ProcessLog,
-    FOutputNotify, AppObj);
-
-  FAmbiTerminalDataSetFormCreator := TerminalFormCreatorCreate
-    (FFormClassNamesSL, LogUsuario, DBMS, Output, ProcessLog,
-    FOutputNotify, AppObj);
-
-{$IFDEF DEBUG}
-  MenuPageControl.ActivePageIndex := 0;
-{$ELSE}
-  MenuPageControl.ActivePageIndex := 0;
-{$ENDIF}
-end;
-
-procedure TConfigModuloBasForm.ConfigAmbiLojasActionExecute(Sender: TObject);
-begin
-  inherited;
-  TabSheetCrie(FAmbiLojasDataSetFormCreator);
-end;
-
-procedure TConfigModuloBasForm.ConfigDBImportAbrirActionExecute
-  (Sender: TObject);
-var
-  iItemIndex: integer;
-  iSelectedImportIndex: integer;
-  oDBImportForm: TDBImportForm;
-begin
-  inherited;
-  ConfigDBImportAbrirAction.Enabled := False;
-  try
-    oDBImportForm := DBImportFormCreate(DBImportOrigemComboBox.ItemIndex);
-    oDBImportForm.ShowModal;
-  finally
-    ConfigDBImportAbrirAction.Enabled := True;
-  end;
-end;
-
-procedure TConfigModuloBasForm.ConfigTerminaisActionExecute(Sender: TObject);
-begin
-  inherited;
-  TabSheetCrie(FAmbiTerminalDataSetFormCreator);
 end;
 
 procedure TConfigModuloBasForm.DBImportPrep;
 begin
-  DBImportOrigemComboBox.Text := '';
-  DBImportOrigemComboBox.Clear;
-end;
-
-procedure TConfigModuloBasForm.ShowTimer_BasFormTimer(Sender: TObject);
-var
-  i: integer;
-  sText: string;
-begin
-  inherited;
-  ClearStyleElements(Self);
-  if AppObj.AppTestesConfig.ModuConf.Ambi.Loja.AutoExec then
-    ConfigAmbiLojasAction.Execute;
-
-  if AppObj.AppTestesConfig.ModuConf.Import.AutoExec then
-  begin
-    MenuPageControl.ActivePage := ConfigImportTabSheet;
-
-    sText := DBImportOrigemComboBox.Text;
-    i := DBImportOrigemComboBox.Items.IndexOf(sText);
-    if i >= 0 then
-    begin
-      DBImportOrigemComboBox.ItemIndex := i;
-      ConfigDBImportAbrirAction.Execute;
-    end;
-  end;
-
-//  ConfigTerminaisAction.Execute;
 end;
 
 procedure TConfigModuloBasForm.TabSheetCrie(pFormCreator: IFormCreator);
-var
-  oTabSheet: TTabSheet;
-  oForm: TForm;
-  sFormClassName: string;
-  iExistenteIndex: integer;
-
-  oFormOwner: TComponent;
-  oAppInfo: IAppInfo;
-  oSisConfig: ISisConfig;
 begin
-  sFormClassName := pFormCreator.FormClassName;
-
-  iExistenteIndex := FFormClassNamesSL.IndexOf(sFormClassName);
-  if iExistenteIndex > -1 then
-  begin
-    oTabSheet := TTabSheet(FFormClassNamesSL.Objects[iExistenteIndex]);
-    PageControl1.ActivePage := oTabSheet;
-    FOutputNotify.Exibir('Opção já aberta');
-    exit;
-  end;
-
-  oTabSheet := TTabSheet.Create(PageControl1);
-  oTabSheet.PageControl := PageControl1;
-  oTabSheet.Name := sFormClassName + 'TabSheet';
-  PageControl1.ActivePage := oTabSheet;
-
-  oFormOwner := oTabSheet;
-
-  oForm := pFormCreator.FormCreate(oFormOwner);
-  oForm.Parent := oTabSheet;
-
-  FFormClassNamesSL.AddObject(sFormClassName, oTabSheet);
-
-  oTabSheet.Caption := pFormCreator.Titulo;
-
-  ClearStyleElements(oTabSheet);
-
-  oForm.Show;
 end;
 
 end.
