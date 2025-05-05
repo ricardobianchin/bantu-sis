cdbf77dd195906f2df0a251f4d77fe721eb57c2d
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-05-05 05:28:25 -0300

    Update TestaDtControl1.dpr

--- Teste/Edit/Data/TestaDtControl/T1/TestaDtControl1.dpr
+++ Teste/Edit/Data/TestaDtControl/T1/TestaDtControl1.dpr
@@ -4,7 +4,7 @@ uses
   Vcl.Forms,
   TestaDtControlForm_u in 'TestaDtControlForm_u.pas' {TestaDtControlForm},
   Sis.UI.Frame.Bas_u in '..\..\..\..\..\Sis\UI\BasForm\Sis.UI.Frame.Bas_u.pas',
-  Sis.UI.Frame.Control.DateTime_u in '..\..\..\..\..\Sis\UI\BasForm\DateTimeFrame\Sis.UI.Frame.Control.DateTime_u.pas',
+  Sis.UI.Frame.Control.DateTime_u in '..\..\..\..\..\Sis\UI\BasForm\DateTimeFrame\Sis.UI.Frame.Control.DateTime_u.pas' {DateTimeFrame},
   Sis.UI.Frame.Bas.Control_u in '..\..\..\..\..\Sis\UI\BasForm\Sis.UI.Frame.Bas.Control_u.pas',
   Sis.Types.strings_u in '..\..\..\..\..\Sis\Types\Sis.Types.strings_u.pas',
   Sis.Types.Utils_u in '..\..\..\..\..\Sis\Types\Sis.Types.Utils_u.pas',
@@ -13,7 +13,8 @@ uses
   Sis.UI.Controls.Utils in '..\..\..\..\..\Sis\UI\Controls\Sis.UI.Controls.Utils.pas',
   Sis.UI.Constants in '..\..\..\..\..\Sis\UI\Sis.UI.Constants.pas',
   sndkey32 in '..\..\..\..\..\Sis\UI\IO\sndkey32.pas',
-  Sis.Types.Integers in '..\..\..\..\..\Sis\Types\Sis.Types.Integers.pas';
+  Sis.Types.Integers in '..\..\..\..\..\Sis\Types\Sis.Types.Integers.pas',
+  Sis.Sis.Constants in '..\..\..\..\..\Sis\Sis\Sis.Sis.Constants.pas';
 
 {$R *.res}
 
