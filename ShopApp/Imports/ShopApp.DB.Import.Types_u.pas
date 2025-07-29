ee7ca03e6ef28f775df888ac530568e2828a6928
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-07-29 15:38:02 -0300

    Shop, MercadoImportSelectItem, Add StrBuscaInicial

--- ShopApp/Imports/ShopApp.DB.Import.Types_u.pas
+++ ShopApp/Imports/ShopApp.DB.Import.Types_u.pas
@@ -6,8 +6,8 @@ uses System.Classes, Sis.Types;
 
 const
   MercadoImportSelectItems: array[1..2] of TSelectItem = (
-    (Id: 1; Descr: 'PLUBASE'),
-    (Id: 2; Descr: 'i9PDV')
+    (Id: 1; Descr: 'PLUBASE'; StrBuscaInicial: ''),
+    (Id: 2; Descr: 'i9PDV'; StrBuscaInicial: '')
   );
 
 implementation
