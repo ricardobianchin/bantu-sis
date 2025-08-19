00b48121d27843ed98eba2c645492c4ab4ee4b03
Ricardo Bianchin <ricardobianchin@gmail.com>
2025-08-19 11:41:05 -0300

    sis, win, cria ShortCutCreator

---
+++ Sis/Win/ShortCutCreator/Sis.Win.ShortCutCreator.PowerShell_u.pas
@@ -0,0 +1,99 @@
+﻿unit Sis.Win.ShortCutCreator.PowerShell_u;
+
+interface
+
+uses Sis.Win.ShortCutCreator, Sis.Win.ShortCutCreator_u, Sis.UI.IO.Output,
+  Sis.UI.IO.Output.ProcessLog;
+
+type
+  TShortCutCreatorPowerShell = class(TShortCutCreator, IShortCutCreator)
+  private
+  public
+    procedure AddScriptFor(pNomeAtalho, pExe, pParams,
+      pStartIn: string); override;
+    function Execute: Boolean; override;
+  end;
+
+implementation
+
+uses System.SysUtils, Sis.Win.Execute, Sis.Types.strings_u, Sis.Win.Factory,
+  Winapi.Windows;
+
+{ TShortCutCreatorPowerShell }
+
+procedure TShortCutCreatorPowerShell.AddScriptFor(pNomeAtalho, pExe, pParams,
+  pStartIn: string);
+begin
+  pStartIn := IncludeTrailingPathDelimiter(pStartIn);
+
+  ScriptSL.Add('# script criado automaticamente. cria atalho');
+  ScriptSL.Add('$WShell = New-Object -ComObject WScript.Shell');
+  ScriptSL.Add('$Shortcut = $WShell.CreateShortcut("' + PastaDesktop +
+    pNomeAtalho + '.lnk")');
+  ScriptSL.Add('$Shortcut.TargetPath = "' + pExe + '"');
+  if pParams <> '' then
+    ScriptSL.Add('$Shortcut.Arguments = "' + pParams + '"');
+  ScriptSL.Add('$Shortcut.WorkingDirectory = "' + pStartIn + '"');
+  ScriptSL.Add('$Shortcut.Save()');
+  ScriptSL.Add('');
+end;
+
+function TShortCutCreatorPowerShell.Execute: Boolean;
+var
+  sNomeScript: string;
+  WinExec: IWinExecute;
+  sLog: string;
+begin
+  ProcessLog.PegueLocal('TShortCutCreatorPowerShell.Execute');
+  try
+    sNomeScript := PastaComandos +
+      StrToNomeArq('PowerShell ' + Assunto) + '.ps1';
+
+    // Salva o script PowerShell no arquivo
+    ScriptSL.SaveToFile(sNomeScript);
+    ProcessLog.RegistreLog('Script salvo: ' + sNomeScript, Now, lptExecExternal,
+      sNomeScript);
+
+    // Cria IWinExecute para executar o script com elevação e captura de saída/erro
+    WinExec := WinExecuteCreate('powershell.exe',
+      '-ExecutionPolicy ByPass -File "' + sNomeScript + '"', PastaComandos, //
+      False, // Não executar ao criar
+      SW_SHOWMINNOACTIVE, //
+      True, // pElevate para runas
+      Output, //
+      ProcessLog, //
+      PastaComandos, //
+      '', // Nome padrão com timestamp (ex.: WinExec.saida.<timestamp>.txt)
+      '' // Nome padrão com timestamp (ex.: WinExec.erro.<timestamp>.txt)
+      );
+
+    // Executa o comando
+    ProcessLog.RegistreLog('vai WinExec.Execute');
+    Result := WinExec.Execute;
+    if Result then
+    begin
+      ProcessLog.RegistreLog('vai WinExec.EspereExecucao');
+      WinExec.EspereExecucao(Output);
+
+      Mens := WinExec.Saida + #13#10 + WinExec.Erro + #13#10;
+
+      // // Exibe resultado, mantendo comportamento original
+      // if WinExec.Erro <> '' then
+      // ShowMessage('Erro no PowerShell: ' + WinExec.Erro)
+      // else
+      // ShowMessage('Execução OK: ' + WinExec.Saida);
+
+      // Opcional: apaga o arquivo de script após execução
+      // DeleteFile(sNomeScript);
+    end
+    else
+    begin
+      Mens := 'Falha ao iniciar execução do script PowerShell';
+      ProcessLog.RegistreLog(Mens, Now, lptExecExternal, sNomeScript);
+    end;
+  finally
+    ProcessLog.RetorneLocal;
+  end;
+end;
+
+end.
---
+++ Sis/Win/ShortCutCreator/Sis.Win.ShortCutCreator_u.pas
@@ -0,0 +1,68 @@
+﻿unit Sis.Win.ShortCutCreator_u;
+
+interface
+
+uses Sis.Win.ShortCutCreator, Sis.UI.IO.Output, Sis.UI.IO.Output.ProcessLog,
+  Sis.Sis.Executavel_u, System.Classes;
+
+type
+  TShortCutCreator = class(TExecutavel, IShortCutCreator)
+  private
+    FMens: string;
+    FScriptSL: TStrings;
+    FAssunto, FPastaComandos, FPastaDesktop: string;
+  protected
+    property ScriptSL: TStrings read FScriptSL;
+    property Assunto: string read FAssunto;
+    property PastaComandos: string read FPastaComandos;
+    property PastaDesktop: string read FPastaDesktop;
+    function GetMens: string; virtual;
+    procedure SetMens(Value: string);
+  public
+    procedure AddScriptFor(pNomeAtalho, pExe, pParams, pStartIn: string);
+      virtual; abstract;
+    function Execute: Boolean; virtual; abstract;
+    property Mens: string read GetMens write SetMens;
+
+    procedure Inicialize(pAssunto, pPastaComandos, pPastaDesktop: string);
+    constructor Create(pOutput: IOutput = nil; pProcessLog: IProcessLog = nil);
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses System.SysUtils, Sis.UI.IO.Files;
+
+{ TShortCutCreator }
+
+constructor TShortCutCreator.Create(pOutput: IOutput; pProcessLog: IProcessLog);
+begin
+  inherited Create(pOutput, pProcessLog);
+  FScriptSL := TStringList.Create;
+end;
+
+destructor TShortCutCreator.Destroy;
+begin
+  FScriptSL.Free;
+  inherited;
+end;
+
+function TShortCutCreator.GetMens: string;
+begin
+  Result := FMens;
+end;
+
+procedure TShortCutCreator.Inicialize(pAssunto, pPastaComandos,
+  pPastaDesktop: string);
+begin
+  FAssunto := pAssunto;
+  FPastaComandos := GarantaPasta(PastaComandos);
+  FPastaDesktop := IncludeTrailingPathDelimiter(pPastaDesktop);
+end;
+
+procedure TShortCutCreator.SetMens(Value: string);
+begin
+  FMens := Value;
+end;
+
+end.
